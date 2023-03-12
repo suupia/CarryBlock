c90c556a61688eaa7bc707d3439587726de8d338
suupia <morita777k@icloud.com>
2023-03-12 16:41:01 +0900

    Improve:IPickerStateにSwitchStateを追加し、リファクタリング

--- Assets/Scripts/PickerController.cs
+++ Assets/Scripts/PickerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 using System.Linq;
 using System.Numerics;
 using UnityEditor;
+using UnityEngine.XR;
 using Vector3 = UnityEngine.Vector3;
+
+
+public interface IPickerContext
+{
+    public void ChangeState(IPickerState state);
+}
+
+public interface IPickerState
+{
+    public abstract void InitProcess();
+    public abstract void Process(IPickerContext state);
+    public abstract bool CanSwitchState();
+    public abstract void SwitchState(IPickerContext state);
+}
+
 
 public class PickerController : MonoBehaviour
 {
     bool isInitialized = false;
 
     // Pure
-    PickerInfo pickerInfo;
-    IPickerState pickerState;
+    private PickerInfo pickerInfo;
+    private PickerContext pickerContext;
+
 
     // Components
     GameObject targetResourceObj;
@@ -26,7 +43,10 @@ public class PickerController : MonoBehaviour
 
         detectionRange = rangeRadius;
 
-        pickerInfo = new PickerInfo(this.gameObject);
+        pickerInfo = new PickerInfo(this.gameObject,detectionRange);
+        pickerContext = new PickerContext();
+
+
 
         pickerInfo.SetPlayerObj(playerObj);
 
@@ -39,30 +59,19 @@ public class PickerController : MonoBehaviour
         isInitialized = true;
     }
 
+
     private void Update()
     {
         if (!isInitialized) return;
 
-        pickerState.Process();
-        if (pickerState.CanSwitchState())
+        pickerContext.currentState.Process(pickerContext);
+        if (pickerContext.currentState.CanSwitchState())
         {
-            if (pickerState.GetType() == typeof(PickerApproachState))
-            {
-                pickerState = new PickerCollectionState(pickerInfo);
-                pickerState.InitProcess();
-            }
-            else if (pickerState.GetType() == typeof(PickerCollectionState))
-            {
-                pickerState = new PickerReturnState(pickerInfo);
-                pickerState.InitProcess();
-            }
-            else if (pickerState.GetType() == typeof(PickerReturnState))
-            {
-                pickerState = new PickerCompleteState(pickerInfo);
-                pickerState.InitProcess();
-            }
+            pickerContext.currentState.SwitchState(pickerContext);
+            pickerContext.currentState.InitProcess();
         }
     }
+
 
     private void Search()
     {
@@ -74,13 +83,13 @@ public class PickerController : MonoBehaviour
         {
             targetResourceObj = resources.ElementAt(0);
             pickerInfo.SetTargetResourceObj(targetResourceObj);
-            pickerState = new PickerApproachState(pickerInfo);
-            pickerState.InitProcess();
+            pickerContext.ChangeState(pickerInfo.approachState);
+            pickerContext.currentState.InitProcess();
         }
         else
         {
-            pickerState = new PickerNotFoundState(pickerInfo);
-            pickerState.InitProcess();
+            pickerContext.ChangeState(pickerInfo.returnToPlayerState);
+            pickerContext.currentState.InitProcess();
         }
 
     }
@@ -98,6 +107,7 @@ public class PickerController : MonoBehaviour
 
 public class PickerInfo
 {
+    // constant fields
     public readonly float acceleration = 10f;
     public readonly float maxVelocity = 30;
 
@@ -105,18 +115,39 @@ public class PickerInfo
     public readonly float collectionTime = 1.5f;
     public readonly float collectOffset = 1;
 
-    public readonly float returnRange = 1;
+    public readonly float returnToHeadquartersRange = 1;
+    public readonly float returnToPlayerRange = 2;
 
+    // singletons
+    public IPickerState approachState { get; private set; }
+    public IPickerState collectionState { get; private set; }
+    public IPickerState returnToHeadquartersState { get; private set; }
+    public IPickerState returnToPlayerState { get; private set; }
+    public IPickerState completeState { get; private set; }
+
+    // components
     public GameObject pickerObj { get; private set; }
     public Rigidbody pickerRd { get; private set; }
     public GameObject playerObj { get; private set; }
     public GameObject targetResourceObj { get; private set; }
     public GameObject headquartersObj { get; private set; }
 
-    public PickerInfo(GameObject pickerObj)
+    // injected fields
+
+    public  float detectionRange { get; private set; }
+
+    public PickerInfo(GameObject pickerObj, float detectionRange)
     {
         this.pickerObj = pickerObj;
         this.pickerRd = pickerObj.GetComponent<Rigidbody>();
+
+        approachState = new PickerApproachState(this);
+        collectionState = new PickerCollectionState(this);
+        returnToHeadquartersState = new PickerReturnToHeadquartersState(this);
+        returnToPlayerState = new PickerReturnToPlayerState(this);
+        completeState = new PickerCompleteState(this);
+
+        this.detectionRange = detectionRange;
     }
     public void SetPlayerObj(GameObject playerObj)
     {
@@ -135,47 +166,118 @@ public class PickerInfo
     }
 }
 
+
+
+public class PickerContext : IPickerContext
+{
+    public IPickerState currentState { get; private set; }
+
+
+    public void ChangeState(IPickerState state)
+    {
+        this.currentState = state;
+    }
+}
 public abstract class PickerAbstractState : IPickerState
 {
     protected PickerInfo info;
-    GameObject targetResourceObj;
-    GameObject headquartersObj;
 
     protected PickerAbstractState(PickerInfo info)
     {
         this.info = info;
     }
 
-    public abstract void InitProcess();
-    public abstract void Process();
-    public abstract bool CanSwitchState();
 
+    public abstract void InitProcess();
+    public abstract void Process(IPickerContext context);
+    public abstract bool CanSwitchState();
+    public abstract void SwitchState(IPickerContext context);
     protected void Move(Vector3 startPos, Vector3 endPos)
     {
         var directionVec = Utility.SetYToZero(endPos - startPos).normalized;
         info.pickerRd.AddForce(info.acceleration * directionVec, ForceMode.Acceleration);
         if (info.pickerRd.velocity.magnitude >= info.maxVelocity) info.pickerRd.velocity = info.maxVelocity * info.pickerRd.velocity.normalized;
     }
-}
-public class PickerNotFoundState : PickerAbstractState
-{
-    public PickerNotFoundState(PickerInfo info) : base(info)
-    {
 
+    protected void Move(Vector3 moveVector)
+    {
+        var directionVec = Utility.SetYToZero(moveVector).normalized;
+        info.pickerRd.AddForce(info.acceleration * directionVec, ForceMode.Acceleration);
+        if (info.pickerRd.velocity.magnitude >= info.maxVelocity) info.pickerRd.velocity = info.maxVelocity * info.pickerRd.velocity.normalized;
+    }
+}
+
+public class PickerSearchState : PickerAbstractState
+{
+    private float timer = 0;
+    private readonly float minSpawnTime = 1;
+    public PickerSearchState(PickerInfo info) : base(info)
+    {
     }
 
     public override void InitProcess()
     {
+        timer = 0;
+    }
+    public override void Process(IPickerContext context)
+    {
+        timer += Time.deltaTime;
+        var moveVector = info.pickerObj.transform.forward;
+        Move(moveVector);
+
+        //‚à‚µresource‚ðŒ©‚Â‚¯‚½‚çó‘Ô‚ð•ÏX
+        Collider[] colliders = Physics.OverlapSphere(info.pickerObj.transform.position, info.detectionRange);
+        var resources = colliders.
+            Where(collider => collider.CompareTag("Resource")).
+            Select(collider => collider.gameObject);
+        if (resources.Any())
+        {
+            info.SetTargetResourceObj(resources.ElementAt(0));
+            context.ChangeState(info.approachState);
+        }
 
     }
-    public override void Process()
+
+    public override bool CanSwitchState()
     {
+        return timer > minSpawnTime;
+    }
+
+    public override void SwitchState(IPickerContext context)
+    {
+        context.ChangeState(info.returnToPlayerState);
+    }
+
+}
+
+public class PickerReturnToPlayerState : PickerAbstractState
+{
+    private float timer = 0;
+    private readonly float minSpawnTime = 1;
+
+    public PickerReturnToPlayerState(PickerInfo info) : base(info)
+    {
+    }
+
+    public override void InitProcess()
+    {
+        timer = 0;
+    }
+    public override void Process(IPickerContext context)
+    {
+        timer += Time.deltaTime;
         Move(info.pickerObj.transform.position, info.playerObj.transform.position);
     }
 
     public override bool CanSwitchState()
     {
-        return false;
+        var vector = Utility.SetYToZero(info.playerObj.transform.position - info.pickerObj.transform.position);
+        return vector.magnitude <= info.returnToPlayerRange && timer > minSpawnTime;
+    }
+
+    public override void SwitchState(IPickerContext context)
+    {
+        context.ChangeState(info.completeState);
     }
 
 }
@@ -190,7 +292,7 @@ public class PickerApproachState : PickerAbstractState
     {
 
     }
-    public override void Process()
+    public override void Process(IPickerContext context)
     {
         Move(info.pickerObj.transform.position, info.targetResourceObj.transform.position);
     }
@@ -199,6 +301,10 @@ public class PickerApproachState : PickerAbstractState
     {
         var vector = Utility.SetYToZero(info.targetResourceObj.transform.position - info.pickerObj.transform.position);
         return vector.magnitude <= info.collectionRange;
+    }
+    public override void SwitchState(IPickerContext context)
+    {
+        context.ChangeState(info.collectionState);
     }
 
 }
@@ -222,7 +328,7 @@ public class PickerCollectionState : PickerAbstractState
         initPos = info.pickerObj.transform.position;
         deltaVector = info.targetResourceObj.transform.position - initPos;
     }
-    public override void Process()
+    public override void Process(IPickerContext context)
     {
         Debug.Log($"CollectProcess()");
         timer += Time.deltaTime;
@@ -246,14 +352,18 @@ public class PickerCollectionState : PickerAbstractState
         return timer >= info.collectionTime;
     }
 
+    public override void SwitchState(IPickerContext context)
+    {
+        context.ChangeState(info.returnToHeadquartersState);
+    }
 
 
 }
 
-public class PickerReturnState : PickerAbstractState
+public class PickerReturnToHeadquartersState : PickerAbstractState
 {
 
-    public PickerReturnState(PickerInfo info) : base(info)
+    public PickerReturnToHeadquartersState(PickerInfo info) : base(info)
     {
     }
 
@@ -261,7 +371,7 @@ public class PickerReturnState : PickerAbstractState
     {
 
     }
-    public override void Process()
+    public override void Process(IPickerContext context)
     {
         Debug.Log($"ReturnProcess()");
         Move(info.pickerObj.transform.position, info.headquartersObj.transform.position);
@@ -270,10 +380,15 @@ public class PickerReturnState : PickerAbstractState
     public override bool CanSwitchState()
     {
         var vector = Utility.SetYToZero(info.headquartersObj.transform.position - info.pickerObj.transform.position);
-        return vector.magnitude <= info.returnRange;
+        return vector.magnitude <= info.returnToHeadquartersRange;
     }
-
+    public override void SwitchState(IPickerContext context)
+    {
+        context.ChangeState(info.completeState);
+    }
 }
+
+
 
 public class PickerCompleteState : PickerAbstractState
 {
@@ -285,23 +400,20 @@ public class PickerCompleteState : PickerAbstractState
     {
 
     }
-    public override void Process()
+    public override void Process(IPickerContext context)
     {
         Debug.Log($"CompleteProcess()");
-        GameObject.Destroy(info.pickerObj);
+        Object.Destroy(info.pickerObj);
     }
 
     public override bool CanSwitchState()
     {
         return false;
     }
+    public override void SwitchState(IPickerContext context)
+    {
+        Debug.LogWarning($"PickerCompleteState.SwitchState(context:{context})");
+    }
 
 }
 
-
-public interface IPickerState
-{
-    public abstract void InitProcess();
-    public abstract void Process();
-    public abstract bool CanSwitchState();
-}
