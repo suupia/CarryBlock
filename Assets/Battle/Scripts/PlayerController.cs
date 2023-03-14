7c5c3194c01a8d54449a4ef2e3755ce22510b687
suupia <morita777k@icloud.com>
2023-03-14 17:35:50 +0900

    Improve:BulletsParentとPickersParentをPlayersオブジェクトの配下へ移動

--- Assets/Battle/Scripts/PlayerController.cs
+++ Assets/Battle/Scripts/PlayerController.cs
@@ -3,16 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject cameraPrefab;
     [SerializeField] GameObject rangeCirclePrefab;
-
-    Transform bulletsParent;
-    [SerializeField] GameObject bulletPrefab;
-    Transform pickersParent;
-    [SerializeField] GameObject pickerPrefab;
 
     [SerializeField] GameObject[] unitPrefabs;
 
@@ -39,26 +35,13 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         var playerObj = Instantiate(unitPrefabs[(int)unitType],gameObject.transform);
-
-        Instantiate(cameraPrefab, playerObj.transform);
-        var rangeCircleGameObj = Instantiate(rangeCirclePrefab, playerObj.transform);
-
-
-        bulletsParent = GameObject.Find("BulletsParent").transform;
-        pickersParent = GameObject.Find("PickersParent").transform;
-
+        var playerCamera  = Instantiate(cameraPrefab, playerObj.transform);
+        var rangeCircleObj = Instantiate(rangeCirclePrefab, playerObj.transform);
 
         info.Init(playerObj);
         playerUnit = new PlayerTank(info);
 
-
-        rangeCircleGameObj.transform.localScale = new Vector3(rangeRadius*2,rangeRadius*2,rangeRadius * 2); // localScale sets the diameter
-        var onTriggerEnterComponent = rangeCircleGameObj.GetComponent<OnTriggerEnterComponent>();
-        Debug.Log($"onTriggerEnterComponent:{onTriggerEnterComponent}");
-        onTriggerEnterComponent.SetOnTriggerEnterAction(playerUnit.OnTriggerRangeCircle);
-
-
-        
+        InitRangeCircleObj(rangeCircleObj);
     }
 
     void Update()
@@ -67,36 +50,22 @@ public class PlayerController : MonoBehaviour
         var verticalInput = Input.GetAxisRaw("Vertical");
         var direction = Vector3.Normalize(new Vector3(horizontalInput, 0, verticalInput));
 
-
-        // related to movement
-
-        ////transform.position = speed * directionVec + transform.position;
-        //playerRd.AddForce(acceleration * directionVec, ForceMode.Acceleration);
-        //if (playerRd.velocity.magnitude >= maxVelocity) playerRd.velocity = maxVelocity * playerRd.velocity.normalized;
-
-        //if (directionVec == Vector3.zero) playerRd.velocity = resistance * playerRd.velocity; //Decelerate when there is no key input
-
         playerUnit.MoveUnit(direction);
-
-        // related to action
 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Left mouse button is clicked.");
             playerUnit.UnitAction();
         }
     }
 
+    void InitRangeCircleObj(GameObject rangeCircleObj)
+    {
+        rangeCircleObj.transform.localScale = new Vector3(rangeRadius * 2, rangeRadius * 2, rangeRadius * 2); // localScale sets the diameter
+        var onTriggerEnterComponent = rangeCircleObj.GetComponent<OnTriggerEnterComponent>();
+        Debug.Log($"onTriggerEnterComponent:{onTriggerEnterComponent}");
+        onTriggerEnterComponent.SetOnTriggerEnterAction(playerUnit.OnTriggerRangeCircle);
 
-    //void OnTriggerRangeCircle(Collider other)
-    //{
-    //    if (other.CompareTag("Enemy"))
-    //    {
-    //        var bulletInitPos = bulletOffset * (other.gameObject.transform.position - playerObj.transform.position).normalized + playerObj.transform.position;
-    //        var bullet = Instantiate(bulletPrefab, bulletInitPos, Quaternion.identity, bulletsParent).GetComponent<BulletController>();
-    //        bullet.Init(other.gameObject);
-    //    }
-    //}
+    }
 
 }
 
@@ -146,7 +115,6 @@ public abstract class PlayerUnit
 
     public abstract void MoveUnit(Vector3 direction);
     public abstract void UnitAction();
-
     public abstract void OnTriggerRangeCircle(Collider other);
 }
 
@@ -169,16 +137,15 @@ public class PlayerTank : PlayerUnit
     {
         // Launch a picker.
         var pickerPos = info. playerObj.transform.position + new Vector3(0, pickerHeight, 0);
-        var picker = MonoBehaviour.Instantiate(info.pickerPrefab, pickerPos, Quaternion.identity, info.pickersParent).GetComponent<PickerController>();
+        var picker = Object.Instantiate(info.pickerPrefab, pickerPos, Quaternion.identity, info.pickersParent).GetComponent<PickerController>();
         picker.Init(info.playerObj.gameObject, info.rangeRadius);
-        Debug.Log($"rangeRadius:{info.rangeRadius}");
     }
     public override void OnTriggerRangeCircle(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
             var bulletInitPos = info. bulletOffset * (other.gameObject.transform.position - info.playerObj.transform.position).normalized + info.playerObj.transform.position;
-            var bullet = MonoBehaviour.Instantiate(info.bulletPrefab, bulletInitPos, Quaternion.identity, info.bulletsParent).GetComponent<BulletController>();
+            var bullet = Object.Instantiate(info.bulletPrefab, bulletInitPos, Quaternion.identity, info.bulletsParent).GetComponent<BulletController>();
             bullet.Init(other.gameObject);
         }
     }
