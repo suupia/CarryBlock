2dfdea9c7c44b8e62d13f8f68cd2f8ea1ebc21b7
suupia <>
2023-04-09 20:53:16 +0900

    GameObjectPool.Get()で必ずGameObjectが取得できるように修正

--- Assets/ObjectPool/PoolTestManager.cs
+++ Assets/ObjectPool/PoolTestManager.cs
@@ -27,32 +27,28 @@ public class PoolTestManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            var find = GameObject.Find("Apple");
+            var find = GameObject.Find("Picker");
             pool.Release(find.gameObject);
         }
         if (Input.GetKeyDown(KeyCode.B))
         {
-            var find = GameObject.Find("Banana");
+            var find = GameObject.Find("Tank");
             pool.Release(find.gameObject);
         }
         
         if(Input.GetKeyDown(KeyCode.C))
         {
-            var find = GameObject.Find("Cherry");
-            pool.Release(find.gameObject);
+            pool.Clear();
         }
-        
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            pool.Clear();;
-        }
+
     }
 }
 
-
+# nullable enable
 public class GameObjectPool
 {
-    readonly List<GameObject> _pool;
+    readonly GameObject[] _pool;
+    readonly List<GameObject> _activeObjects = new List<GameObject>();
     readonly int _poolSize;
 
     public int CountActive => _pool.Count(obj => obj.activeSelf);
@@ -62,14 +58,16 @@ public class GameObjectPool
     public GameObjectPool(Transform parent, int poolSize = 20, string name = "Pool")
     {
         if (poolSize <= 0)
+        {
             throw new ArgumentException("Parent does not have any of these objects.", nameof(poolSize));
-        _pool = new List<GameObject>();
+        }
+        _pool = new GameObject[poolSize];
         for (int i = 0; i< poolSize; i++)
         {
             var obj = new GameObject(name);
             obj.transform.SetParent(parent);
             obj.SetActive(false);
-            _pool.Add(obj);
+            _pool[i] = obj;
         }
     }
 
@@ -77,8 +75,16 @@ public class GameObjectPool
     {
         var obj = _pool.FirstOrDefault(obj => !obj.activeSelf);
         if (obj == null)
-            throw new ArgumentException("Inactive pooled objects are missing.", nameof(_poolSize));
-        obj.SetActive(true);
+        {
+            obj = _activeObjects.First();
+            SetActiveFalseFirst(obj);
+            SetActiveTrue(obj);
+            Debug.LogWarning($"There were not enough pooled objects, so I reactivated the oldest object that became active. poolSize = {_pool.Length}");
+        }
+        else
+        {
+            SetActiveTrue(obj);
+        }
         return obj;
     }
 
@@ -95,7 +101,7 @@ public class GameObjectPool
         if (obj.activeSelf == false)
             throw new ArgumentException("Attempting to release an object that is already inactive", nameof(obj));
 
-        obj.SetActive(false);
+        SetActiveFalseLast(obj);
     }
 
     public void Clear()
@@ -104,5 +110,22 @@ public class GameObjectPool
         {
             obj.SetActive(false);
         }
+    }
+
+    void SetActiveTrue(GameObject obj)
+    {
+        obj.SetActive(true);
+        _activeObjects.Add(obj);
+    }
+    void SetActiveFalseFirst(GameObject obj)
+    {
+        obj.SetActive(false);
+        _activeObjects.RemoveAt(0);
+    }
+    
+    void SetActiveFalseLast(GameObject obj)
+    {
+        obj.SetActive(false);
+        _activeObjects.RemoveAt(_activeObjects.Count()-1);
     }
 }
