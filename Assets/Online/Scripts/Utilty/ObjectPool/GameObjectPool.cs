5a56542ad158957fd5eed5ea0afbbda2799205ce
suupia <>
2023-04-10 12:16:43 +0900

    Move:ObjectPoolフォルダの位置をOnline/Scripts/Utiltyに移動

--- Assets/Online/Scripts/Utilty/ObjectPool/GameObjectPool.cs
+++ Assets/Online/Scripts/Utilty/ObjectPool/GameObjectPool.cs
@@ -1,48 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Pool;
 using System;
-using System.Data;
-using Object = System.Object;
-
-public class PoolTestManager : MonoBehaviour
-{
-    GameObjectPool pool;
-    [SerializeField] Transform parent;
-
-    void Start()
-    {
-        pool = new GameObjectPool(parent, 20, "Picker");
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-           var child =   pool.Get();
-           Debug.Log(child.name);
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            var find = GameObject.Find("Picker");
-            pool.Release(find.gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            var find = GameObject.Find("Tank");
-            pool.Release(find.gameObject);
-        }
-        
-        if(Input.GetKeyDown(KeyCode.C))
-        {
-            pool.Clear();
-        }
-
-    }
-}
+using System.Linq;
 
 # nullable enable
 public class GameObjectPool
