4564d609955c5441d75d0e86abed404c8998979c
suupia <>
2023-04-12 13:43:39 +0900

    D_Sc: Battleフォルダに関係するスクリプトを削除

--- Assets/Online/UI/SelectUnitUI.cs
+++ Assets/Online/UI/SelectUnitUI.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SelectUnitUI : MonoBehaviour
-{
-    [SerializeField] GameObject selectUnitUIObj;
-    [SerializeField] Transform selectUnitButtonsParent;
-    PlayerSpawner playerSpawner;
-
-    void Start()
-    {
-        playerSpawner = new PlayerSpawner();
-
-        var buttons = selectUnitButtonsParent.GetComponentsInChildren<Button>();
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            int index = i;
-            buttons[i].onClick.AddListener(() => OnClick(index));
-        }
-    }
-
-    void OnClick(int index)
-    {
-        Debug.Log($"number:{index}");
-        var unitType = (PlayerController.UnitType)index;
-        playerSpawner.SetUnitType((PlayerController.UnitType)index);
-
-        playerSpawner.SpawnPlayer();
-
-        CloseUI();
-    }
-
-    void CloseUI()
-    {
-        selectUnitUIObj.gameObject.SetActive(false);
-    }
-}
+// public class SelectUnitUI : MonoBehaviour
+// {
+//     [SerializeField] GameObject selectUnitUIObj;
+//     [SerializeField] Transform selectUnitButtonsParent;
+//     PlayerSpawner playerSpawner;
+//
+//     void Start()
+//     {
+//         playerSpawner = new PlayerSpawner();
+//
+//         var buttons = selectUnitButtonsParent.GetComponentsInChildren<Button>();
+//         for (int i = 0; i < buttons.Length; i++)
+//         {
+//             int index = i;
+//             buttons[i].onClick.AddListener(() => OnClick(index));
+//         }
+//     }
+//
+//     void OnClick(int index)
+//     {
+//         Debug.Log($"number:{index}");
+//         var unitType = (PlayerController.UnitType)index;
+//         playerSpawner.SetUnitType((PlayerController.UnitType)index);
+//
+//         playerSpawner.SpawnPlayer();
+//
+//         CloseUI();
+//     }
+//
+//     void CloseUI()
+//     {
+//         selectUnitUIObj.gameObject.SetActive(false);
+//     }
+// }
