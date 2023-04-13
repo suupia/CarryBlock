15b78422a57073c8369b0a74062a55af9c1659cf
tyPhoon <typhoonmoon1022@gmail.com>
2023-04-13 14:30:39 +0900

    U_Sc: interfaceのPublic化

--- Assets/Animations/Scripts/Interfaces.cs
+++ Assets/Animations/Scripts/Interfaces.cs
@@ -1,33 +1,30 @@
 using UnityEngine;
 
-namespace Animations.Scripts
+public interface IAnimatorMove
 {
-    internal interface IAnimatorMove
-    {
-        /// <summary>
-        /// Needs some param
-        /// </summary>
-        /// <param name="direction">Used for manage body animation</param>
-        void OnMove(Vector3 direction);
-    }
+    /// <summary>
+    /// Needs some param
+    /// </summary>
+    /// <param name="direction">Used for manage body animation</param>
+    void OnMove(Vector3 direction);
+}
 
-    internal interface IAnimatorDead
-    {
-        void OnDead();
-    }
+public interface IAnimatorDead
+{
+    void OnDead();
+}
 
-    internal interface IAnimatorSpawn
-    {
-        void OnSpawn();
-    }
+public interface IAnimatorSpawn
+{
+    void OnSpawn();
+}
 
-    internal interface IAnimatorAttack
-    {
-        void OnAttack();
-    }
+public interface IAnimatorAttack
+{
+    void OnAttack();
+}
 
-    internal interface IAnimatorPlayerUnit: IAnimatorAttack, IAnimatorDead, IAnimatorMove, IAnimatorSpawn
-    {
-        void OnMainAction();
-    }
+public interface IAnimatorPlayerUnit: IAnimatorAttack, IAnimatorDead, IAnimatorMove, IAnimatorSpawn
+{
+    void OnMainAction();
 }
