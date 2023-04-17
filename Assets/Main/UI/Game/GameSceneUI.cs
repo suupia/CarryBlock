3bf51b1e3b332597fa17b1eb61ab514af9c67d9e
suupia <>
2023-04-17 22:04:59 +0900

    A_Un: ScoreTextの追加

---
+++ Assets/Main/UI/Game/GameSceneUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace  Main.UI
+{
+    public class GameSceneUI : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _scoreText;
+    }
+}
