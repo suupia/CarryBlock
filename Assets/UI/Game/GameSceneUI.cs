04ef663c86e98b3e9dd26f0c74ea7d026ee59cb6
suupia <>
2023-04-18 22:27:51 +0900

    F_Sc: GameSceneUIのscoreをUInt16に変更

--- Assets/UI/Game/GameSceneUI.cs
+++ Assets/UI/Game/GameSceneUI.cs
@@ -13,7 +13,7 @@ namespace  UI
         [SerializeField] TextMeshProUGUI _scoreText;
         [SerializeField] TextMeshProUGUI _waveTimerText;
 
-        [Networked] int _score { get; set; }
+        [Networked] UInt16 score { get; set; }
 
         [SerializeField] WaveTimer _waveTimer;
         ResourceAggregator _resourceAggregator;
@@ -30,15 +30,15 @@ namespace  UI
         public override void FixedUpdateNetwork()
         {
             if(!HasStateAuthority)return;
-            _score = _resourceAggregator.getAmount; // クライアントに反映させるためにNetworkedで宣言した変数に値を代入する
-            Debug.Log($"_score : {_score}");
+            score = (UInt16)_resourceAggregator.getAmount; // クライアントに反映させるためにNetworkedで宣言した変数に値を代入する
+            Debug.Log($"_score : {score}, runner : {Runner}");
         }
 
 
         public override void Render()
         {
-             Debug.Log($"_score : {_score}");
-            _scoreText.text = $"Score : {_score}";
+             Debug.Log($"_score : {score}, runner : {Runner}");
+            _scoreText.text = $"Score : {score}";
             _waveTimerText.text = $"Time : {Mathf.Floor(_waveTimer.getRemainingTime)}";
         }
         
