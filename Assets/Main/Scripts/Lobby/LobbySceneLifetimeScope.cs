4a33507730a869a8c35e38729675760b92fe2972
suupia <morita777k@icloud.com>
2023-04-20 14:08:53 +0900

    A_Sc: LobbySceneLifetimeScopeを追加

---
+++ Assets/Main/Scripts/Lobby/LobbySceneLifetimeScope.cs
@@ -0,0 +1,17 @@
+using VContainer;
+using VContainer.Unity;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Main.VContainer
+{
+    public sealed class LobbySceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<ResourceAggregator>(Lifetime.Singleton);
+            builder.Register<GameContext>(Lifetime.Singleton);
+            builder.Register<WaveTimer>(Lifetime.Singleton);
+        }
+    }
+}
