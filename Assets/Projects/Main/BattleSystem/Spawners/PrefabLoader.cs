de991515a6d5a79e5f73080208bcf0749e13dec4
suupia <morita777k@icloud.com>
2023-05-16 10:09:11 +0900

    U_Sc: Spawnersフォルダを整理

---
+++ Assets/Projects/Main/BattleSystem/Spawners/PrefabLoader.cs
@@ -0,0 +1,36 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace Main
+{
+    public interface IPrefabLoader<out T> where T : Object
+    {
+        T Load(string prefabName);
+    }
+
+
+// Other classes can be created by implementing IPrefabLoader, such as PrefabLoaderFromAssetBundle and PrefabLoaderFromStreamingAssets.
+    public class PrefabLoaderFromResources<T> : IPrefabLoader<T> where T : Object
+    {
+        readonly string _folderPath;
+
+        public PrefabLoaderFromResources(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public T Load(string prefabName)
+        {
+            var result = Resources.Load<T>(_folderPath + "/" + prefabName);
+            if (result == null)
+            {
+                Debug.LogError($"Failed to load prefab. folderPath ={_folderPath+"/"+prefabName} prefabName = {prefabName}");
+                return null;
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
