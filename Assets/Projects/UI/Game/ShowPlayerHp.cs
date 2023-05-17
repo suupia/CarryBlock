1e78c711ea441f4a7642d2abf9065d2f4c37979b
suupia <morita777k@icloud.com>
2023-05-17 13:51:48 +0900

    A_Sc: RunnerMangerにIsReadyを追加

--- Assets/Projects/UI/Game/ShowPlayerHp.cs
+++ Assets/Projects/UI/Game/ShowPlayerHp.cs
@@ -10,21 +10,29 @@ using UnityEngine.Serialization;
 # nullable enable
 public class ShowPlayerHp : MonoBehaviour
 {
+    NetworkRunnerManager _runnerManger;
    [SerializeField] AbstractNetworkPlayerController? playerController;
    [SerializeField] TextMeshProUGUI? hpText;
    Vector3 offset = new Vector3(0, 1.5f, 0);
-
-
+   
     RectTransform? _rectTransform;
     async void  Start()
     {
         _rectTransform = gameObject.GetComponent<RectTransform>();
+        _runnerManger = FindObjectOfType<NetworkRunnerManager>();
+        
     }
     void Update()
     {
+        if(!_runnerManger.IsReady)return;
         _rectTransform.position 
             = RectTransformUtility.WorldToScreenPoint(Camera.main, playerController.gameObject.transform.position + offset);
         hpText.text = $"HP = {playerController.PlayerStruct.Hp.ToString()}";
+    }
+
+    public void SetCamera(Camera playerCamera)
+    {
+        
     }
     
     // 動的に生成されたカメラを設定する必要がある....
