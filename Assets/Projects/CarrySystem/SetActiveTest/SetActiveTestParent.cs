daa4882860698d3340db6d65db0da65e89d54682
suupia <morita777k@icloud.com>
2023-06-15 00:05:31 +0900

    F_Sc: PlayerOperation.MainActionで切り替えるように変更

--- Assets/Projects/CarrySystem/SetActiveTest/SetActiveTestParent.cs
+++ Assets/Projects/CarrySystem/SetActiveTest/SetActiveTestParent.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Fusion;
 using Fusion.Sockets;
+using Nuts.NetworkUtility.Inputs.Scripts;
 using UnityEngine;
 
 namespace Carry.CarrySystem.SetActiveTest
@@ -18,9 +19,10 @@ namespace Carry.CarrySystem.SetActiveTest
 
             if (GetInput(out NetworkInputData input))
             {
-                if (input.Buttons.WasPressed(PreButtons, PlayerOperation.ChangeSetActive))
+                if (input.Buttons.WasPressed(PreButtons, PlayerOperation.MainAction))
                 {
                     targetObject.SetActive(!targetObject.activeSelf);
+                    Debug.Log($"Toggled Active -> {targetObject.activeSelf}");
                 }
                 
                 PreButtons = input.Buttons;
@@ -45,104 +47,5 @@ namespace Carry.CarrySystem.SetActiveTest
         }
     }
 
-    public enum PlayerOperation
-    {
-        ChangeSetActive,
-    }
 
-
-    public class LocalInputPoller : INetworkRunnerCallbacks
-    {
-        // Local variable to store the input polled.
-        NetworkInputData localInput;
-
-        public void OnInput(NetworkRunner runner, NetworkInput input)
-        {
-            localInput = new NetworkInputData();
-            localInput.Horizontal = Input.GetAxisRaw("Horizontal");
-            localInput.Vertical = Input.GetAxisRaw("Vertical");
-            localInput.Buttons.Set(PlayerOperation.ChangeSetActive, Input.GetKey(KeyCode.N));
-
-            input.Set(localInput);
-
-            // Reset the input struct to start with a clean slate
-            // when polling for the next tick
-            localInput = default;
-        }
-
-
-        #region Ignore
-
-        public void OnConnectedToServer(NetworkRunner runner)
-        {
-        }
-
-        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-        {
-        }
-
-        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request,
-            byte[] token)
-        {
-        }
-
-        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-        {
-        }
-
-        public void OnDisconnectedFromServer(NetworkRunner runner)
-        {
-        }
-
-        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-        {
-        }
-
-        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-        {
-        }
-
-        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-        {
-        }
-
-        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-        {
-        }
-
-        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
-        {
-        }
-
-        public void OnSceneLoadDone(NetworkRunner runner)
-        {
-        }
-
-        public void OnSceneLoadStart(NetworkRunner runner)
-        {
-        }
-
-        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-        {
-        }
-
-        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-        {
-        }
-
-        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-        {
-        }
-
-        #endregion
-    }
-
-
-    public struct NetworkInputData : INetworkInput
-    {
-        [Networked] public float Horizontal { get; set; }
-        [Networked] public float Vertical { get; set; }
-        public NetworkButtons Buttons;
-
-    }
 }
