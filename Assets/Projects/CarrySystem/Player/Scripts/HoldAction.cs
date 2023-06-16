e128370a2705bca6c64f6c7e76ccd1614571ab14
suupia <morita777k@icloud.com>
2023-06-16 22:59:53 +0900

    U_Sc: HoldActionをリファクタリング

--- Assets/Projects/CarrySystem/Player/Scripts/HoldAction.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/HoldAction.cs
@@ -14,7 +14,7 @@ namespace Carry.CarrySystem.Player.Scripts
         PlayerInfo _info;
         EntityGridMap _map;
         IHoldActionPresenter? _presenter;
-        public bool IsHoldingRock { get; set; } = false;
+        bool _isHoldingRock = false;
 
         public HoldAction(IHoldActionPresenter? presenter)
         {
@@ -31,18 +31,9 @@ namespace Carry.CarrySystem.Player.Scripts
         {
             var transform = _info.playerObj.transform;
             
-            Debug.Log($"ものを拾ったり、置いたりします");
-
-            // 自身のGridPosを表示
-            var gridPos = GridConverter.WorldPositionToGridPosition(transform.position);
-            Debug.Log($"Player GridPos: {gridPos}");
-
-            // 前方のGridPosを表示
-            var forward = transform.forward;
-            var direction = new Vector2(forward.x, forward.z);
-            var gridDirection = GridConverter.WorldDirectionToGridDirection(direction);
-            var forwardGridPos = gridPos + gridDirection;
-            Debug.Log($"Player Forward GridPos: {forwardGridPos}");
+            // 前方のGridPosを取得
+            var forwardGridPos = GetForwardGridPos(transform);
+            // Debug.Log($"Player Forward GridPos: {forwardGridPos}");
             
             // そのGridPosにRockがあるかどうかを確認
             var index = _map.GetIndexFromVector(forwardGridPos);
@@ -50,50 +41,57 @@ namespace Carry.CarrySystem.Player.Scripts
             var rock = _map.GetSingleEntity<Rock>(forwardGridPos);
             if (rock == null)
             {
-                Debug.Log($"Rockがありません");
-                
-                // 持っていたら置く処理をする
-                if (IsHoldingRock)
+                Debug.Log($"forwardGridPos: {forwardGridPos}にRockはありません");
+                if (_isHoldingRock)
                 {
-                    // 持っていいるRockを置く
-                    _map.AddEntity<Rock>(forwardGridPos, new Rock(Rock.Kind.Kind1, forwardGridPos));
-
-                    IsHoldingRock = false;
-                    
-                    _presenter?.PutDownRock();
-                    
-                    return;
+                    PutDownRock(forwardGridPos);
                 }
                 else
                 {
                     // 何もしない
                 }
-
             }
             else
             {
-                Debug.Log($"Rockがあります！！！");
-                if (IsHoldingRock)
+                Debug.Log($"forwardGridPos: {forwardGridPos}にRockがあります");
+                if (_isHoldingRock)
                 {
                     // 何もしない
-                    return;
                 }
                 else
                 {
-                    // ドメインのRockを削除（内部のプレゼンターを通して見た目も変わる）
-                    _map.RemoveEntity(forwardGridPos, rock);
-                    
-                    // プレイヤーがRockを持つようにする
-                    IsHoldingRock = true;
-                    
-                    _presenter?.PickUpRock();
-
+                    PickUpRock(forwardGridPos, rock);
                 }
             }
 
-            // アイテムがある場合は、アイテムを拾う
+        }
+        Vector2Int GetForwardGridPos(Transform transform)
+        {
+            var gridPos = GridConverter.WorldPositionToGridPosition(transform.position);
+            var forward = transform.forward;
+            var direction = new Vector2(forward.x, forward.z);
+            var gridDirection = GridConverter.WorldDirectionToGridDirection(direction);
+            return gridPos + gridDirection;
+        }
 
-            // アイテムがない、かつ、アイテムを持っているときは、アイテムを置く
+        void PickUpRock(Vector2Int forwardGridPos, Rock rock)
+        {
+            // ドメインのRockを削除（内部のプレゼンターを通して見た目も変わる）
+            _map.RemoveEntity(forwardGridPos, rock);
+            
+            _isHoldingRock = true;
+                    
+            _presenter?.PickUpRock();
+        }
+
+        void PutDownRock(Vector2Int forwardGridPos)
+        {
+            // 新しくRockを生成して置く
+            _map.AddEntity<Rock>(forwardGridPos, new Rock(Rock.Kind.Kind1, forwardGridPos));
+
+            _isHoldingRock = false;
+                    
+            _presenter?.PutDownRock();
         }
     }
 }
