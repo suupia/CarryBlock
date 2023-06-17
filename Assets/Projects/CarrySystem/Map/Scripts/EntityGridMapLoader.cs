1dfe48275b6eaebf71440820436bfd1835494ab4
suupia <morita777k@icloud.com>
2023-06-17 11:12:18 +0900

    U_Sc: DefaultEntityGridMapDataを別ファイルに切り出し

--- Assets/Projects/CarrySystem/Map/Scripts/EntityGridMapLoader.cs
+++ Assets/Projects/CarrySystem/Map/Scripts/EntityGridMapLoader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Carry.CarrySystem.Entity.Scripts;
 using Carry.CarrySystem.Spawners;
 using UnityEngine;
+using Carry.CarrySystem.Map.MapData;
 
 #nullable enable
 
@@ -76,74 +77,5 @@ namespace Carry.CarrySystem.Map.Scripts
             return File.Exists(GetFilePath(index));
         }
     }
-    
-    public class DefaultEntityGridMapData : EntityGridMapData
-    {
-        // 適当に作っている
-        // エクセルからデータを読み込めるようになるまではこれを使用する
-        
-        readonly int _length;
-        public DefaultEntityGridMapData()
-        {
-            // 親クラスのフィールドを書き換えていることに注意
-            width = 20;
-            height = 11;
-            _length = width * height;
-            groundRecords = new GroundRecord[_length];
-            rockRecords = new RockRecord[_length];
 
-            FillAll();
-            PlaceGround();
-            PlaceRock();
-        }
-        
-        void FillAll()
-        {
-            for (int i = 0; i < _length; i++)
-            {
-                groundRecords[i] = new GroundRecord();
-                rockRecords[i] = new RockRecord();
-            }
-        }
-
-        void PlaceGround()
-        {
-            // すべてのマスに対してGroundを配置する
-            for (int i = 0; i < _length; i++)
-            {
-                groundRecords[i].kind = Ground.Kind.Kind1;
-            }
-        }
-
-        void PlaceRock()
-        {
-            List<int> rockPosList = new List<int>();
-            
-            rockPosList.Add(0); //原点に置く
-            for (int i = 1 ;i < height; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    rockPosList.Add(i * width -1);
-                    rockPosList.Add(i * width - 3);
-                }
-                else
-                {
-                    rockPosList.Add(i * width -2);
-                    rockPosList.Add(i * width - 4);
-                }
-
-            }
-            // Debug.Log($"rockPosList : {string.Join(",", rockPosList)}");
-
-            for (int i = 0; i < _length; i++)
-            {
-                if (rockPosList.Contains(i))
-                {
-                    rockRecords[i].kind = Rock.Kind.Kind1;
-                }
-            }
-
-        }
-    }
 }
