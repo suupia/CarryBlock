136a26a5eb6f304553a92669dfe9a884ac13ea80
suupia <morita777k@icloud.com>
2023-07-14 14:20:24 +0900

    mousePosOnGroundを計算する処理を追加

---
+++ Assets/Projects/EditMapSystem/EditMap/Scripts/EditMapInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Carry.CarrySystem.Map.Scripts;
+using UnityEngine;
+
+namespace Carry.EditMapSystem.EditMap.Scripts
+{
+    public class EditMapInput : MonoBehaviour
+    {
+        void Update()
+        {
+            var mouseXYPos = Input.mousePosition; // xy座標であることに注意
+            var cameraHeight = Camera.main.transform.position.y;
+            var mousePosOnGround = Camera.main.ScreenToWorldPoint(new Vector3(mouseXYPos.x, mouseXYPos.y, cameraHeight));
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                var clickGridPos = GridConverter.WorldPositionToGridPosition(mousePosOnGround);
+                Debug.Log($"mousePosOnGround : {mousePosOnGround},  clickGridPos: {clickGridPos}");
+            }
+        }
+    }
+}
