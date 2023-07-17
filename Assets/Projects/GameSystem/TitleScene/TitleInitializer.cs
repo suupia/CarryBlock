499f793346e3ad4ff033bb9a4b40d5c2e6584741
tyPhoon <typhoonmoon1022@gmail.com>
2023-07-18 00:14:55 +0900

    タイトル画面のJoinボタンをJoinAsClientとJoinAsHostボタンに分割し、実装。それに伴い、タイトル画面のUIを管理するTitleUIManagerの追加

--- Assets/Projects/GameSystem/TitleScene/TitleInitializer.cs
+++ Assets/Projects/GameSystem/TitleScene/TitleInitializer.cs
@@ -1,23 +1,24 @@
-using System;
+using Fusion;
 using Nuts.BattleSystem.Scripts;
 using Nuts.NetworkUtility.NetworkRunnerManager.Scripts;
 using UnityEngine;
+using Assert = UnityEngine.Assertions.Assert;
 
 namespace Nuts.BattleSystem.TitleScene.Scripts
 {
     public class TitleInitializer : MonoBehaviour
     {
-        //Get roomName from UI component.
-        public string RoomName { get; set; }
-        
         bool _isStarted; // StarGameWithRoomName() is called only once.
 
         // Called by UI Button
-        public async void StartGameWithRoomName()
+        public async void StartGame(string roomName, GameMode gameMode)
         {
-            if(_isStarted) return;
+            if (_isStarted) return;
             var runnerManager = FindObjectOfType<NetworkRunnerManager>();
-            await runnerManager.AttemptStartScene(RoomName);
+            Assert.IsNotNull(runnerManager, "NetworkRunnerManagerをシーンに配置してください");
+
+            await runnerManager.AttemptStartScene(roomName, gameMode);
+
             Debug.Log("Transitioning to LobbySceneTestRoom");
             SceneTransition.TransitioningScene(runnerManager.Runner, SceneName.LobbyScene);
             _isStarted = true;
---
+++ Assets/Projects/GameSystem/TitleScene/TitleUIManager.cs
@@ -0,0 +1,37 @@
+using Fusion;
+using Nuts.BattleSystem.TitleScene.Scripts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Assert = UnityEngine.Assertions.Assert;
+
+/// <summary>
+///     タイトルのUI要素を管理する
+///     ボタンのコールバックの設定などを行う
+/// </summary>
+public class TitleUIManager : MonoBehaviour
+{
+    [SerializeField] Button joinAsHostButton;
+
+    [SerializeField] Button joinAsClientButton;
+
+    [SerializeField] TMP_InputField roomNameInputField;
+
+    string RoomName => roomNameInputField.text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsNotNull(joinAsHostButton);
+        Assert.IsNotNull(joinAsClientButton);
+        Assert.IsNotNull(roomNameInputField);
+
+        var titleInitializer = FindObjectOfType<TitleInitializer>();
+        Assert.IsNotNull(titleInitializer, "TitleInitializerをシーンに配置してください");
+
+        joinAsClientButton.onClick.AddListener(() => titleInitializer.StartGame(RoomName, GameMode.Client));
+        //GameMode.Hostとして扱うかは未定。仮でAutoHostOrClientに設定
+        //もし、GameMode.Hostかつ同じルーム名で始めた場合はStartGameExceptionがthrowされる
+        joinAsHostButton.onClick.AddListener(() => titleInitializer.StartGame(RoomName, GameMode.AutoHostOrClient));
+    }
+}
