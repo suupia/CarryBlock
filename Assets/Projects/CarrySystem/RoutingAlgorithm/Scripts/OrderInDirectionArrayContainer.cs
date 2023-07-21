c4b252e77e09319e585f0cb78226cfefee8484c7
suupia <morita777k@icloud.com>
2023-07-21 18:21:42 +0900

    OrderInDirectionのメンバをreadonlyに変更

--- Assets/Projects/CarrySystem/RoutingAlgorithm/Scripts/OrderInDirectionArrayContainer.cs
+++ Assets/Projects/CarrySystem/RoutingAlgorithm/Scripts/OrderInDirectionArrayContainer.cs
@@ -8,7 +8,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 6 4 2
         // 8 * 1
         // 7 5 3 の優先順位で判定していく
-        public static Vector2Int[] CounterClockwiseStartingRightDirections = new Vector2Int[]
+        public static readonly Vector2Int[] CounterClockwiseStartingRightDirections = new Vector2Int[]
         {
             Vector2Int.right, Vector2Int.right + Vector2Int.up, Vector2Int.right + Vector2Int.down, Vector2Int.up,
             Vector2Int.down, Vector2Int.left + Vector2Int.up, Vector2Int.left + Vector2Int.down, Vector2Int.left
@@ -17,7 +17,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 7 5 3
         // 8 * 1
         // 6 4 2 の優先順位で判定していく
-        public static Vector2Int[] ClockwiseStartingRightDirections = new Vector2Int[]
+        public static readonly Vector2Int[] ClockwiseStartingRightDirections = new Vector2Int[]
         {
             Vector2Int.right, Vector2Int.right + Vector2Int.down, Vector2Int.right + Vector2Int.up, Vector2Int.down,
             Vector2Int.up, Vector2Int.left + Vector2Int.down, Vector2Int.left + Vector2Int.up, Vector2Int.left
@@ -26,7 +26,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 8 6 4
         // 7 * 2
         // 5 3 1 の優先順位で判定していく
-        public static Vector2Int[] CounterClockwiseStartingRightDownDirections = new Vector2Int[]
+        public static readonly Vector2Int[] CounterClockwiseStartingRightDownDirections = new Vector2Int[]
         {
             Vector2Int.right + Vector2Int.down, Vector2Int.right, Vector2Int.down, Vector2Int.right + Vector2Int.up,
             Vector2Int.left + Vector2Int.down, Vector2Int.up, Vector2Int.left, Vector2Int.left + Vector2Int.up
@@ -35,7 +35,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 8 7 5
         // 6 * 3
         // 4 2 1 の優先順位で判定していく
-        public static Vector2Int[] ClockwiseStartingRightDownDirections = new Vector2Int[]
+        public static readonly Vector2Int[] ClockwiseStartingRightDownDirections = new Vector2Int[]
         {
             Vector2Int.right + Vector2Int.down, Vector2Int.down, Vector2Int.right, Vector2Int.left + Vector2Int.down,
             Vector2Int.right + Vector2Int.up, Vector2Int.left, Vector2Int.up, Vector2Int.left + Vector2Int.up
@@ -44,7 +44,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 1 3 5
         // 2 * 7
         // 4 6 8 の優先順位で判定していく
-        public static Vector2Int[] CounterClockwiseStartingLeftUpDirections = new Vector2Int[]
+        public static readonly Vector2Int[] CounterClockwiseStartingLeftUpDirections = new Vector2Int[]
         {
             Vector2Int.left + Vector2Int.up, Vector2Int.left, Vector2Int.up, Vector2Int.left + Vector2Int.down,
             Vector2Int.right + Vector2Int.up, Vector2Int.down, Vector2Int.right, Vector2Int.right + Vector2Int.down
@@ -53,7 +53,7 @@ namespace Carry.CarrySystem.RoutingAlgorithm.Scripts
         // 1 2 4
         // 3 * 6
         // 5 7 8 の優先順位で判定していく
-        public static Vector2Int[] ClockwiseStartingLeftUpDirections = new Vector2Int[]
+        public static readonly Vector2Int[] ClockwiseStartingLeftUpDirections = new Vector2Int[]
         {
             Vector2Int.left + Vector2Int.up, Vector2Int.up, Vector2Int.left, Vector2Int.right + Vector2Int.up,
             Vector2Int.left + Vector2Int.down, Vector2Int.right, Vector2Int.down, Vector2Int.right + Vector2Int.down
