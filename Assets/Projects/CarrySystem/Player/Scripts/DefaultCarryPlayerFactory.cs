6a4f1db44426faac037edf509f7571db045e5ae7
Kinogas <qingmeizhongben5@gmail.com>
2023-07-22 10:02:00 +0900

    TsukamotoMove.csを作成

--- Assets/Projects/CarrySystem/Player/Scripts/DefaultCarryPlayerFactory.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/DefaultCarryPlayerFactory.cs
@@ -14,7 +14,7 @@ namespace Carry.CarrySystem.Player.Scripts
         
         public ICharacter Create(PlayerColorType colorType)
         {
-            var move = new QuickTurnMove();
+            var move = new TsukamotoMove();
             var action = new HoldAction(_resolver);
             var character = new Character(move, action);
             return character;
---
+++ Assets/Projects/CarrySystem/Player/Scripts/TsukamotoMove.cs
@@ -0,0 +1,54 @@
+﻿using Carry.CarrySystem.Player.Info;
+using Carry.CarrySystem.Player.Interfaces;
+using UnityEngine;
+
+namespace Carry.CarrySystem.Player.Scripts
+{
+    public class TsukamotoMove : ICharacterMove
+    {
+        PlayerInfo _info;
+        float _acceleration = 60f;
+        float _maxVelocity = 16f;
+        float _rotateTime  = 0.4f;
+
+        public void Setup(PlayerInfo info)
+        {
+            _info = info;
+            _acceleration = info.acceleration;
+            _maxVelocity = info.maxVelocity;
+            _rotateTime = info.targetRotationTime;
+        }
+        public void Move(Vector3 input)
+        {
+            var transform = _info.playerObj.transform;
+            var rb = _info.playerRb;
+            
+            var deltaAngle = Vector3.SignedAngle(transform.forward, input, Vector3.up);
+            // Debug.Log($"deltaAngle = {deltaAngle}");
+
+            if (input != Vector3.zero)
+            {
+                // Rotate if there is a difference of more than Epsilon degrees
+                if (Mathf.Abs(deltaAngle) >= float.Epsilon)
+                {
+                    var rotateQuaternion = Quaternion.Euler(0, deltaAngle, 0);
+                    rb.MoveRotation(rb.rotation * rotateQuaternion);
+
+                    // ToDo: DoTweenで回転させる
+                    // var rotateAngle = new Vector3(0, deltaAngle, 0);
+                    // _rb.DORotate(rotateAngle, rotateTime)
+                    //     .SetEase(Ease.OutExpo)
+                    //     .Play();
+
+                }
+                
+                // ToDo: _rb.MovePosition()で素早く移動させる
+                rb.AddForce(_acceleration * input, ForceMode.Acceleration);
+
+                if (rb.velocity.magnitude >= _maxVelocity)
+                    rb.velocity = _maxVelocity * rb.velocity.normalized;
+            }
+        }
+    }
+    
+    }
