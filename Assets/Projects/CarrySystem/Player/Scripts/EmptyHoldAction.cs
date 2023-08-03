2a34e7f0253d99d87f44c79e9f02ba916dac6093
suupia <morita777k@icloud.com>
2023-08-03 12:50:22 +0900

    EmptyHoldAction.csを作成

---
+++ Assets/Projects/CarrySystem/Player/Scripts/EmptyHoldAction.cs
@@ -0,0 +1,28 @@
+﻿using Carry.CarrySystem.Player.Info;
+using Carry.CarrySystem.Player.Interfaces;
+using UnityEngine;
+
+namespace Carry.CarrySystem.Player.Scripts
+{
+    public class EmptyHoldAction : ICharacterHoldAction
+    {
+        public void SetHoldPresenter(IHoldActionPresenter presenter)
+        {
+            
+        }
+
+        public void Setup(PlayerInfo info)
+        {
+        }
+
+        public void Reset()
+        {
+            
+        }
+
+        public void Action()
+        {
+            Debug.Log($"EmptyHoldAction: Action");
+        }
+    }
+}
---
+++ Assets/Projects/CarrySystem/Player/Scripts/EmptyHoldActionPlayerFactory.cs
@@ -0,0 +1,24 @@
+﻿using Carry.CarrySystem.Cart.Scripts;
+using Carry.CarrySystem.Player.Interfaces;
+using VContainer;
+
+namespace Carry.CarrySystem.Player.Scripts
+{
+    public class EmptyHoldActionPlayerFactory : ICarryPlayerFactory
+    {
+        readonly IObjectResolver _resolver;
+        [Inject]
+        public EmptyHoldActionPlayerFactory(IObjectResolver resolver)
+        {
+            _resolver = resolver;
+        }
+        
+        public ICharacter Create(PlayerColorType colorType)
+        {
+            var move = new CorrectlyStopMove();
+            var action = new EmptyHoldAction();
+            var character = new Character(move, action);
+            return character;
+        }
+    }
+}
