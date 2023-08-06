e03670f60db7d94952d854f4af3ebf53c660daca
suupia <morita777k@icloud.com>
2023-08-06 17:08:56 +0900

    仮のHeavyBlockを作成

---
+++ Assets/Projects/CarrySystem/Block/Scripts/HeavyBlock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Carry.CarrySystem.Block.Interfaces;
+using Carry.CarrySystem.Entity.Interfaces;
+using Carry.CarrySystem.Map.Scripts;
+using UnityEngine;
+#nullable  enable
+
+namespace Carry.CarrySystem.Block.Scripts
+{
+    // JSONファイルに書き出すためにSerializableをつける
+    [Serializable]
+    public record HeavyBlockRecord
+    {
+        public HeavyBlock.Kind[] kinds = new HeavyBlock.Kind[10];
+    }
+
+    public class HeavyBlock : IBlock
+    {
+        public Vector2Int GridPosition { get; set; }
+        public int MaxPlacedBlockCount { get; } = 2;
+        public BasicBlock.Kind KindValue { get; }
+
+        public enum Kind
+        {
+            None,
+            Kind1,
+        }
+
+        public HeavyBlock(BasicBlock.Kind kind, Vector2Int gridPosition)
+        {
+            KindValue = kind;
+            GridPosition = gridPosition;
+        }
+
+        public bool CanPickUp()
+        {
+            return true;  // basicが持ち上げられない状況はない
+        }
+
+        public void  PickUp()
+        {
+            // 特になし
+        }
+
+        public bool CanPutDown(IList<IBlock> blocks)
+        {
+            var diffList = blocks.Select(x => x.GetType() != this.GetType());
+            Debug.Log($"forward different block count: {diffList.Count()}, list : {string.Join(",", diffList)}");
+            if (blocks.Count(x => x.GetType() != this.GetType()) > 0)
+            {
+                Debug.Log($"Basicと違う種類のBlockがあるため置けません");
+                return false;
+            }
+
+            if (blocks.Count >= MaxPlacedBlockCount) return false; // 現在持ち上げているBlockの設置上限数以上ならfalse
+
+            return true;
+        }
+        
+        public void PutDown() 
+        {
+            // 特になし
+        }
+    }
+}
