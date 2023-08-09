99812b2d8ddb6a112a32c0761c69aa0db6da02ad
tyPhoon <typhoonmoon1022@gmail.com>
2023-08-09 16:07:57 +0900

    タイトルシーンの改修、基本的な部分のみ追加、適用

--- Assets/Projects/GameSystem/TitleScene/TitleUIManager.cs
+++ Assets/Projects/GameSystem/TitleScene/TitleUIManager.cs
@@ -1,6 +1,7 @@
 using Fusion;
 using Projects.BattleSystem.TitleScene.Scripts;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using Assert = UnityEngine.Assertions.Assert;
@@ -11,11 +12,19 @@ using Assert = UnityEngine.Assertions.Assert;
 /// </summary>
 public class TitleUIManager : MonoBehaviour
 {
+    [Header("Title")] [SerializeField] Button soloPlayButton;
+
+    [SerializeField] Button multiPlayButton;
+    [SerializeField] Button optionButton;
+    [SerializeField] Button musicRoomButton;
+    [SerializeField] Button quitButton;
+
+
+    [Header("JoinPanel")] [SerializeField] GameObject joinPanel;
     [SerializeField] Button joinAsHostButton;
-
     [SerializeField] Button joinAsClientButton;
-
     [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] Button backButton;
 
     string RoomName => roomNameInputField.text;
 
@@ -29,9 +38,32 @@ public class TitleUIManager : MonoBehaviour
         var titleInitializer = FindObjectOfType<TitleInitializer>();
         Assert.IsNotNull(titleInitializer, "TitleInitializerをシーンに配置してください");
 
+
         joinAsClientButton.onClick.AddListener(() => titleInitializer.StartGame(RoomName, GameMode.Client));
         //GameMode.Hostとして扱うかは未定。仮でAutoHostOrClientに設定
         //もし、GameMode.Hostかつ同じルーム名で始めた場合はStartGameExceptionがthrowされる
         joinAsHostButton.onClick.AddListener(() => titleInitializer.StartGame(RoomName, GameMode.AutoHostOrClient));
+
+
+        soloPlayButton.interactable = false;
+
+        multiPlayButton.onClick.AddListener(() => joinPanel.SetActive(true));
+
+        optionButton.interactable = false;
+
+        musicRoomButton.interactable = false;
+
+        quitButton.onClick.AddListener(Quit);
+
+        backButton.onClick.AddListener(() => joinPanel.SetActive(false));
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false; //ゲームプレイ終了
+#else
+        Application.Quit();//ゲームプレイ終了
+#endif
     }
 }
