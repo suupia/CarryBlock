d23d870af781e3100fd87a865e7ba1f314aa9311
suupia <morita777k@icloud.com>
2023-08-11 15:51:04 +0900

    色の変更が次のシーンに引き継がれるように実装

---
+++ Assets/Projects/CarrySystem/Player/Scripts/PlayerCharacterHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Carry.CarrySystem.Player.Scripts;
+using Fusion;
+
+namespace Carry.CarrySystem.Player.Scripts
+{
+    public class PlayerCharacterHolder
+    {
+        readonly Dictionary<PlayerRef, PlayerColorType> _colorDictionary = new Dictionary<PlayerRef, PlayerColorType>();
+
+        public PlayerColorType GetPlayerColorType(PlayerRef playerRef)
+        {
+            if (_colorDictionary.TryGetValue(playerRef, out PlayerColorType colorType))
+            {
+                return colorType;
+            }
+            return (PlayerColorType)0;
+        }
+        public void SetColor(PlayerRef playerRef, PlayerColorType colorType)
+        {
+            _colorDictionary[playerRef] = colorType;
+        }
+    }
+}
---
+++ Assets/Projects/GameSystem/Scripts/RootScope.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Carry.CarrySystem.Player.Interfaces;
+using Carry.CarrySystem.Player.Scripts;
+using Carry.CarrySystem.Spawners;
+using Fusion;
+using Projects.BattleSystem.LobbyScene.Scripts;
+using Projects.BattleSystem.Scripts;
+using Projects.Utility.Scripts;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace  Carry.GameSystem.Scripts
+{
+    public class RootScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<PlayerCharacterHolder>(Lifetime.Singleton);
+
+        }
+    }
+
+}
