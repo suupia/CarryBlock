90ab8d120a8e626da80ba5fe51ca80daeb8f03d1
suupia <morita777k@icloud.com>
2023-08-19 00:31:58 +0900

    PlayerAnimatorPresenterNetにNetworkedプロパティを追加

--- Assets/Projects/CarrySystem/Player/Scripts/PlayerAnimatorPresenterNet.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/PlayerAnimatorPresenterNet.cs
@@ -10,13 +10,31 @@ namespace Carry.CarrySystem.Player.Scripts
     [RequireComponent(typeof(AbstractNetworkPlayerController))]
     public class PlayerAnimatorPresenterNet : NetworkBehaviour, IPlayerAnimatorPresenter
     {
-        Animator? _animator;
-
+        public enum MovementState
+        {
+            Idle,
+            InWalk,
+            InDash,
+        }
         public struct PresentData : INetworkStruct
         {
-            // [Networked] public PlayerBlockPresenterNet.BlockType HoldingBlockType { get; set; } // enumは共有できない(?)ので、int16で送る
+            [Networked] public int PickUpCount { get; set; }
+            [Networked] public int PutDownCount { get; set; }
+            [Networked] public int ReceiveCount { get; set; }
+            [Networked] public int PassCount { get; set; }
+            [Networked] public  MovementState MovementState { get; set; }
+            [Networked] public bool IsHoldingBlock { get; set; }
         }
+        [Networked] public ref PresentData PresentDataRef => ref MakeRef<PresentData>();
 
+        // 差分を判定して、アニメーションを発火させる
+        int _pickUpCount;
+        int _putDownCount;
+        int _receiveCount;
+        int _passCount;
+
+        Animator? _animator;
+        
         public void Init(ICharacter character)
         {
             Debug.Log($"PlayerAnimatorPresenterNet Init");
@@ -30,85 +48,88 @@ namespace Carry.CarrySystem.Player.Scripts
 
         public override void Render()
         {
+            if (_animator == null)
+            {
+                Debug.LogError($"_animator is null");
+                return;
+            }
+            if (PresentDataRef.PickUpCount > _pickUpCount)
+            {
+                _animator.SetTrigger("PickUp");
+                _pickUpCount = PresentDataRef.PickUpCount;
+            }
+            if(PresentDataRef.PutDownCount > _putDownCount)
+            {
+                _animator.SetTrigger("PutDown");
+                _putDownCount = PresentDataRef.PutDownCount;
+            }
+            if(PresentDataRef.ReceiveCount > _receiveCount)
+            {
+                _animator.SetTrigger("Receive");
+                _receiveCount = PresentDataRef.ReceiveCount;
+            }
+            if(PresentDataRef.PassCount > _passCount)
+            {
+                _animator.SetTrigger("Pass");
+                _passCount = PresentDataRef.PassCount;
+            }
+
+            switch (PresentDataRef.MovementState)
+            {
+                case MovementState.Idle:
+                    _animator.SetBool("InWalk", false);
+                    _animator.SetBool("InDash", false);
+                    break;
+                case MovementState.InWalk:
+                    _animator.SetBool("InWalk", true);
+                    _animator.SetBool("InDash", false);
+                    break;
+                case MovementState.InDash:
+                    _animator.SetBool("InWalk", false);
+                    _animator.SetBool("InDash", true);
+                    break;
+            }
+            _animator.SetBool("IsHoldingBlock", PresentDataRef.IsHoldingBlock);
         }
 
         public void PickUpBlock(IBlock block)
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetTrigger("PickUp");
-            _animator.SetBool("IsHoldingBlock", true);
+            PresentDataRef.PickUpCount++;
+            PresentDataRef.IsHoldingBlock = true;
         }
 
         public void PutDownBlock()
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetTrigger("PutDown");
-            _animator.SetBool("IsHoldingBlock", false);
+            PresentDataRef.PutDownCount++;
+            PresentDataRef.IsHoldingBlock = false;
         }
         
         public void ReceiveBlock(IBlock block)
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetTrigger("Receive");
-            _animator.SetBool("isHoldingBlock", false);
+            PresentDataRef.ReceiveCount++;
+            PresentDataRef.IsHoldingBlock = false;
         }
 
         public void PassBlock()
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetTrigger("Pass");
-            _animator.SetBool("isHoldingBlock", false);
+            PresentDataRef.PassCount++;
+            PresentDataRef.IsHoldingBlock = false;
         }
 
 
         public void Idle()
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetBool("InWalk", false);
-            _animator.SetBool("InDash", false);
+            PresentDataRef.MovementState = MovementState.Idle;
         }
 
         public void Walk()
         {
-            Debug.Log($"Walk Animation");
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetBool("InWalk", true);
-            _animator.SetBool("InDash", false);
+            PresentDataRef.MovementState = MovementState.InWalk;
         }
 
         public void Dash()
         {
-            if (_animator == null)
-            {
-                Debug.LogError($"_animator is null");
-                return;
-            }
-            _animator.SetBool("InWalk", false);
-            _animator.SetBool("InDash", true);
+            PresentDataRef.MovementState = MovementState.InDash;
         }
     }
 }
