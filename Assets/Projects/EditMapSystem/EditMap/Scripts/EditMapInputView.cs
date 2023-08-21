d7a409c39a12c5b97b45d05a6b1299d8e6d03a68
suupia <morita777k@icloud.com>
2023-08-21 20:26:08 +0900

    EditMapInputViewをヒエラルキー上において反映

--- Assets/Projects/EditMapSystem/EditMap/Scripts/EditMapInputView.cs
+++ Assets/Projects/EditMapSystem/EditMap/Scripts/EditMapInputView.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Carry.EditMapSystem.EditMap.Scripts
 {
     public class EditMapInputView : MonoBehaviour
     {
-        [SerializeField]
-        EditMapInput editMapInput;
-        [SerializeField] TMPro.TextMeshProUGUI blockTypeText;
+        [SerializeField] EditMapInput editMapInput;
+        [SerializeField] TextMeshProUGUI blockTypeText;
 
         void Update()
         {
