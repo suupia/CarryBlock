b45991aa126806f22ce344c9052d14e90ec69cff
suupia <morita777k@icloud.com>
2023-09-06 23:14:09 +0900

    DashEffectSetterをNetworkBehaviourに変更

--- Assets/Projects/VFX/DashEffectSetter.cs
+++ Assets/Projects/VFX/DashEffectSetter.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fusion;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.VFX;
 [RequireComponent(typeof(VisualEffect))]
-public class DashEffectSetter : MonoBehaviour
+public class DashEffectSetter : NetworkBehaviour
 {
+    public  struct DashEffectData : INetworkStruct
+    {
+        public bool IsDashing;
+    }
+    [Networked] ref DashEffectData Data => ref MakeRef<DashEffectData>();
+    
     VisualEffect _dashEffect;
     void Start()
     {
@@ -14,16 +21,38 @@ public class DashEffectSetter : MonoBehaviour
         _dashEffect.SendEvent("Stop");
     }
     
-    void Update()
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.A))
+    //     {
+    //         _dashEffect.SendEvent("Start");
+    //     }
+    //
+    //     if (Input.GetKeyUp(KeyCode.A))
+    //     {
+    //         _dashEffect.SendEvent("Stop");
+    //     }
+    // }
+
+    public override void Render()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Data.IsDashing)
         {
             _dashEffect.SendEvent("Start");
         }
-
-        if (Input.GetKeyUp(KeyCode.A))
+        else
         {
             _dashEffect.SendEvent("Stop");
         }
+    }
+
+    public void OnStart()
+    {
+        Data.IsDashing = true;
+    }
+    
+    public void OnStop()
+    {
+        Data.IsDashing = false;
     }
 }
