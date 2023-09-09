cae5b923f5aaf943baf6560b0ce0599c123ed7e4
suupia <morita777k@icloud.com>
2023-09-09 11:01:33 +0900

    TransparencyShaderとテストシーンを追加

---
+++ Assets/Projects/3DCG/Shader/WhiteLight/TransparencyTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TransparencyTest : MonoBehaviour
+{
+    [SerializeField] GameObject block;
+    [SerializeField] Slider whiteRatioSlider;
+    Material _material;
+    
+    void Start()
+    {
+        _material = block.GetComponent<Renderer>().material;
+    }
+    
+    void Update()
+    {
+        // ShaderのNameとReferenceが異なることに注意
+        _material.SetFloat("_TransparencyRatio", whiteRatioSlider.value);
+    }
+}
