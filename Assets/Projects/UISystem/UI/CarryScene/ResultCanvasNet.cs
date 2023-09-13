1c71daf16651eb646e52b92ca2248f34491d92a4
suupia <morita777k@icloud.com>
2023-09-13 23:35:33 +0900

    ResultCanvasNet.csを追加

---
+++ Assets/Projects/UISystem/UI/CarryScene/ResultCanvasNet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+namespace Carry.UISystem.UI.CarryScene
+{
+    public class ResultCanvasNet : NetworkBehaviour
+    {
+
+        
+    }
+}
