7c1a9552447a9288f00f5c87e1b2a3de73e1bd0d
suupia <morita777k@icloud.com>
2023-09-15 11:12:52 +0900

    CustomButton.csを追加

---
+++ Assets/Projects/UISystem/UI/CustomButton.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Projects.UISystem.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class CustomButton : UIBehaviour
+    {
+        
+        [SerializeField] AudioClip clickSound;
+        Button _button;
+        AudioSource _audioSource;
+        
+        protected override void Awake()
+        {
+            _button = GetComponent<Button>();
+            _audioSource = FindObjectOfType<AudioSource>();
+            
+            // audioSourceを取得せずに、 Playのときに　 SoundManager.Instance.Play(clickSound);　という処理にしてもよいかも
+        }
+        
+        public void AddListenerWithSound(UnityAction action)
+        {
+            _button.onClick.AddListener(() =>
+            {
+                action();
+               _audioSource.PlayOneShot(clickSound);
+            });
+        }
+    }
+}
