84d8a0d8f4f86c17950ea775710919f1acdbfebd
suupia <morita777k@icloud.com>
2023-09-15 14:10:16 +0900

    LobbyCanvasUINet.csを作成

---
+++ Assets/Projects/UISystem/UI/LobbyScene/LobbyCanvasUINet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using Projects.BattleSystem.LobbyScene.Scripts;
+using Projects.UISystem.UI;
+using UnityEngine;
+
+#nullable enable
+
+namespace Carry.UISystem.UI.CarryScene
+{
+    public class LobbyCanvasUINet : NetworkBehaviour
+    {
+        [SerializeField] CustomButton startButton = null!;
+
+        public override void Spawned()
+        {
+            if (!HasStateAuthority)
+            {
+                startButton.gameObject.SetActive(false);
+            }
+            
+            var lobbyInitializer = FindObjectOfType<LobbyInitializer>();
+            startButton.AddListener(() =>
+            {
+                lobbyInitializer.TransitionToGameScene();
+            });
+        }
+    }
+}
