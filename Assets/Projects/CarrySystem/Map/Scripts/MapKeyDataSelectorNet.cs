e995c57f8acd697be663b68c60f36f38cae42142
suupia <morita777k@icloud.com>
2023-09-19 18:19:05 +0900

    Stageごとに異なるマップを読み込むようにMapKeyDataSelectorNetを実装

---
+++ Assets/Projects/CarrySystem/Map/Scripts/MapKeyDataSelectorNet.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+namespace Carry.CarrySystem.Map.Scripts
+{
+    // You should this class to be a prefab
+    public class MapKeyDataSelectorNet : NetworkBehaviour
+    {
+        [SerializeField] List<MapKeyDataNet> mapKeyDataNetList = null!;
+
+        public IReadOnlyList<MapKeyData> SelectMapKeyDataList(int index)
+        {
+            if (index < 0 || index >= mapKeyDataNetList.Count)
+            {
+                Debug.LogError($"index:{index} is out of range");
+                return mapKeyDataNetList[0].MapKeyDataList;
+            }
+            return mapKeyDataNetList[index].MapKeyDataList;
+        }
+    }
+}
