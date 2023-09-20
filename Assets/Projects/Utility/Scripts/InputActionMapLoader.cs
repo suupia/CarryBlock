26d2f71d1898b8abf567747bbbae14cda9893d76
suupia <morita777k@icloud.com>
2023-09-20 20:34:32 +0900

    InputActionMapLoader.csを作成

---
+++ Assets/Projects/Utility/Scripts/InputActionMapLoader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+namespace Projects.Utility.Scripts
+{
+    public class InputActionMapLoader
+    {
+        public InputActionMap InputActionMap { get; private set; }
+        public  InputActionMapLoader()
+        {
+            Load();
+        }
+
+        void Load()
+        {
+            var loader =
+                new ScriptableObjectLoaderFromAddressable<InputActionAsset>("InputActionAssets/PlayerInputAction");
+            
+            (var inputActionAsset ,var handler) = loader.Load();
+            Assert.IsNotNull(inputActionAsset, "InputActionを設定してください。Pathが間違っている可能性があります");
+
+            InputActionMap = inputActionAsset.FindActionMap("Default");
+            // InputActionMap.Enable();
+
+            loader.Release(handler);
+        }
+
+    }
+}
