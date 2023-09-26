30fabacdc1c598ced95d195b6798a7e23d099ad4
Kinogas <qingmeizhongben5@gmail.com>
2023-09-26 21:39:46 +0900

    プレイヤーがカートを追従するようにする途中

--- Assets/Projects/CarrySystem/Player/Scripts/Move/PlayerFollowMovingCart.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/Move/PlayerFollowMovingCart.cs
@@ -1,13 +1,24 @@
-﻿#nullable enable
+﻿using Carry.CarrySystem.Cart.Scripts;
+using Carry.CarrySystem.Player.Info;
+using Fusion;
 using UnityEngine;
+using VContainer;
+#nullable enable
 
 namespace Carry.CarrySystem.Player.Scripts
 {
-    public class PlayerFollowMovingCart
+    public class PlayerFollowMovingCart : NetworkBehaviour
     {
+        CarryPlayerContainer _carryPlayerContainer = null!;
+
+        public void Construct(CarryPlayerContainer carryPlayerContainer)
+        {
+            _carryPlayerContainer = carryPlayerContainer;
+        }
         public void FollowMovingCart()
             {
-                Debug.Log("aiueo");
+                CartControllerNet cart = FindObjectOfType<CartControllerNet>(); 
+                Debug.Log("Player is following the cart.");
             }
     }
 }
