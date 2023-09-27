0ac63db3fd51be1e61162ff2c1bb8f36bdc8bad7
Kinogas <qingmeizhongben5@gmail.com>
2023-09-27 23:46:43 +0900

    プレイヤーがカートを追従するようにする途中２

--- Assets/Projects/CarrySystem/Player/Scripts/Move/PlayerFollowMovingCart.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/Move/PlayerFollowMovingCart.cs
@@ -1,5 +1,8 @@
-﻿using Carry.CarrySystem.Cart.Scripts;
+﻿using System;
+using Carry.CarrySystem.Cart.Scripts;
 using Carry.CarrySystem.Player.Info;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Fusion;
 using UnityEngine;
 using VContainer;
@@ -9,16 +12,47 @@ namespace Carry.CarrySystem.Player.Scripts
 {
     public class PlayerFollowMovingCart : NetworkBehaviour
     {
-        CarryPlayerContainer _carryPlayerContainer = null!;
+        readonly CarryPlayerContainer _carryPlayerContainer;
+        CancellationTokenSource? _cancellationTokenSource;
 
-        public void Construct(CarryPlayerContainer carryPlayerContainer)
+        [Inject]
+        public PlayerFollowMovingCart(
+            CarryPlayerContainer carryPlayerContainer
+        )
         {
             _carryPlayerContainer = carryPlayerContainer;
         }
+
         public void FollowMovingCart()
+        {
+            // CancellationToken ct = this.GetCancellationTokenOnDestroy();
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+            DelayAsync(token).Forget();
+        }
+
+        private async UniTask DelayAsync(CancellationToken token)
+        {
+            try
             {
-                CartControllerNet cart = FindObjectOfType<CartControllerNet>(); 
-                Debug.Log("Player is following the cart.");
+                CartControllerNet cart = FindObjectOfType<CartControllerNet>();
+
+                foreach (CarryPlayerControllerNet player in _carryPlayerContainer.PlayerControllers)
+                {
+                    Transform playerTransform = player.transform;
+                    Transform cartTransform = cart.transform;
+
+                    playerTransform.position = cartTransform.position;
+                }
+                
+                await UniTask.Delay(TimeSpan.FromSeconds(5), cancellationToken: token);
+
+            }catch (OperationCanceledException)
+            {
+                // Do nothing
             }
+
+        }
+        //UnityEngine.Object.
     }
 }
