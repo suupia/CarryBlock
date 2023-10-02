97489aef3762766465e156e2283d5657d254b4a6
suupia <morita777k@icloud.com>
2023-10-03 06:29:00 +0900

    Itemタグを追加しカートと接触した時にログが出るように実装

--- Assets/Projects/CarrySystem/Item/Scripts/ItemControllerNet.cs
+++ Assets/Projects/CarrySystem/Item/Scripts/ItemControllerNet.cs
@@ -1,9 +1,13 @@
 ﻿using Fusion;
+using UnityEngine;
 
 namespace Projects.CarrySystem.Item.Scripts
 {
     public class ItemControllerNet : NetworkBehaviour
     {
-        
+        public void OnGain()
+        {
+            Debug.Log($"Gain Item {gameObject.name} pos:{transform.position}");
+        }
     }
 }
