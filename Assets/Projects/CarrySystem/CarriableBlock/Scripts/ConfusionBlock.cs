62a6ae4eb8741da247ffc625f91aedb2511f2d7b
suupia <morita777k@icloud.com>
2023-10-03 21:13:27 +0900

    ConfusionBlock.csを追加

---
+++ Assets/Projects/CarrySystem/CarriableBlock/Scripts/ConfusionBlock.cs
@@ -0,0 +1,67 @@
+#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Carry.CarrySystem.Block.Interfaces;
+using Carry.CarrySystem.CarriableBlock.Interfaces;
+using Carry.CarrySystem.Player.Interfaces;
+using UnityEngine;
+
+namespace Carry.CarrySystem.CarriableBlock.Scripts
+{    // JSONファイルに書き出すためにSerializableをつける
+    [Serializable]
+    public record ConfusionBlockRecord
+    {
+        public ConfusionBlock.Kind[] kinds = new ConfusionBlock.Kind[10];
+    }
+    public class ConfusionBlock : IBlock
+    {
+        public Vector2Int GridPosition { get; set; }
+        public int MaxPlacedBlockCount { get; } = 1;
+        public Kind KindValue { get; }
+
+        public enum Kind
+        {
+            None,
+            Kind1,
+        }
+
+        public ConfusionBlock(Kind kind, Vector2Int gridPosition)
+        {
+            KindValue = kind;
+            GridPosition = gridPosition;
+        }
+
+        public bool CanPickUp()
+        {
+            return true;  // basicが持ち上げられない状況はない
+        }
+
+        public void  PickUp(ICharacter character)
+        {
+            // 移動速度を遅くする
+            character.SwitchToSlowMove();
+        }
+
+        public bool CanPutDown(IList<ICarriableBlock> placedBlocks)
+        {
+            var diffList = placedBlocks.Select(x => x.GetType() != this.GetType());
+            Debug.Log($"forward different block count: {diffList.Count()}, list : {string.Join(",", diffList)}");
+            if (placedBlocks.Count(x => x.GetType() != this.GetType()) > 0)
+            {
+                Debug.Log($"Basicと違う種類のBlockがあるため置けません");
+                return false;
+            }
+
+            if (placedBlocks.Count >= MaxPlacedBlockCount) return false; // 現在持ち上げているBlockの設置上限数以上ならfalse
+
+            return true;
+        }
+        
+        public void PutDown(ICharacter character) 
+        {
+            // 移動速度を元に戻す
+            character.SwitchToRegularMove();
+        }
+    }
+}
