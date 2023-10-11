fad8d757bfcc0695e09265a1821051cad1684ba1
suupia <morita777k@icloud.com>
2023-10-11 18:47:15 +0900

    EntityPresenterLocal.csを実装

--- Assets/Projects/CarrySystem/Map/Scripts/EntityPresenterLocal.cs
+++ Assets/Projects/CarrySystem/Map/Scripts/EntityPresenterLocal.cs
@@ -21,22 +21,6 @@ namespace Carry.CarrySystem.Map.Scripts
 {
     public class EntityPresenterLocal : MonoBehaviour, IEntityPresenter
     {
-        // struct PresentData : INetworkStruct
-        // {
-        //     public int BasicBlockCount;
-        //     public int UnmovableBlockCount;
-        //     public int HeavyBlockCount;
-        //     public int FragileBlockCount;
-        //     public int ConfusionBlockCount;
-        //     public int CannonBlockCount;
-        //     public int TreasureCoinCount;
-        //     public Direction CannonDirection;
-        //     public int SpikeCount;
-        // }
-
-        // [Networked] ref PresentData PresentDataRef => ref MakeRef<PresentData>();
-
-        // このぐらいなら、PrefabLoadするまでもなく直接アタッチした方がよい
         [SerializeField] GameObject basicBlockView = null!;
         [SerializeField] GameObject doubleBasicBlockView = null!;
         [SerializeField] GameObject unmovableBlockView = null!;
@@ -47,191 +31,199 @@ namespace Carry.CarrySystem.Map.Scripts
         [SerializeField] GameObject confusionBlockView = null!;
         [SerializeField] GameObject treasureCoinView = null!;
         [SerializeField] GameObject cannonBlockView = null!;
-        // Direction _cannonDirectionLocal;
+        Direction _cannonDirectionLocal;
         [SerializeField] GameObject spikeView = null!;
-
+        
+        int _basicBlockCount;
+        int _unmovableBlockCount;
+        int _heavyBlockCount;
+        int _fragileBlockCount;
+        int _confusionBlockCount;
+        int _cannonBlockCount;
+        int _treasureCoinCount;
+        Direction _cannonDirection;
+        int _spikeCount;
+        
         public void DestroyPresenter()
         {
             Destroy(this);
         }
 
-         void Update()
-        {
-            // ToDo: 個数で例外を投げる設計でよいのか？　プレゼンターは何個置けるかを知っている必要ないのでは？
-            
-            // // UnmovableBlock
-            // unmovableBlockView.SetActive(PresentDataRef.UnmovableBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"RockCount : {PresentDataRef.UnmovableBlockCount}")
-            // });
-            // doubleUnmovableBlockView.SetActive(PresentDataRef.UnmovableBlockCount switch
-            // {
-            //     0 or 1 => false,
-            //     2 => true,
-            //     _ => throw new InvalidOperationException($"RockCount : {PresentDataRef.UnmovableBlockCount}")
-            // });
-            //
-            // // BasicBlock
-            // basicBlockView.SetActive(PresentDataRef.BasicBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"BasicBlockCount : {PresentDataRef.BasicBlockCount}")
-            // });
-            // doubleBasicBlockView.SetActive(PresentDataRef.BasicBlockCount switch
-            // {
-            //     0 or 1 => false,
-            //     2 => true,
-            //     _ => throw new InvalidOperationException($"BasicBlockCount : {PresentDataRef.BasicBlockCount}")
-            // });
-            //
-            // // HeavyBlock
-            // heavyBlockView.SetActive(PresentDataRef.HeavyBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"HeavyBlockCount : {PresentDataRef.HeavyBlockCount}")
-            // });
-            // doubleHeavyBlockView.SetActive(PresentDataRef.HeavyBlockCount switch
-            // {
-            //     0 or 1 => false,
-            //     2 => true,
-            //     _ => throw new InvalidOperationException($"HeavyBlockCount : {PresentDataRef.HeavyBlockCount}")
-            // });
-            //
-            // // FragileBlock
-            // fragileBlockView.SetActive(PresentDataRef.FragileBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"FragileBlockCount : {PresentDataRef.FragileBlockCount}")
-            // });
-            //
-            // // ConfusionBlock
-            // confusionBlockView.SetActive(PresentDataRef.ConfusionBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"ConfusionBlockCount : {PresentDataRef.ConfusionBlockCount}")
-            // });
-            //
-            // // TreasureCoinBlock
-            // treasureCoinView.SetActive(PresentDataRef.TreasureCoinCount switch
-            // {
-            //     1 => true,
-            //     _ => false
-            // });
-            //
-            // // CannonBlock
-            // cannonBlockView.SetActive(PresentDataRef.CannonBlockCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"CannonBlockCount : {PresentDataRef.CannonBlockCount}")
-            // });
-            // if (_cannonDirectionLocal != PresentDataRef.CannonDirection)
-            // {
-            //     _cannonDirectionLocal = PresentDataRef.CannonDirection;
-            //     // Viewの方を回転させても反映されないので、親を回転させる（たぶんNetwork関連のコンポーネントのせい）
-            //     cannonBlockView.transform.parent.Rotate(0,CalcRotationAmount(_cannonDirectionLocal),0);
-            // }
-            //
-            // // Spike
-            // spikeView.SetActive(PresentDataRef.SpikeCount switch
-            // {
-            //     0 or 2 => false,
-            //     1 => true,
-            //     _ => throw new InvalidOperationException($"SpikeCount : {PresentDataRef.SpikeCount}")
-            // });
-        }
+void Update()
+{
+    // UnmovableBlock
+    unmovableBlockView.SetActive(_unmovableBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"RockCount : {_unmovableBlockCount}")
+    });
+    doubleUnmovableBlockView.SetActive(_unmovableBlockCount switch
+    {
+        0 or 1 => false,
+        2 => true,
+        _ => throw new InvalidOperationException($"RockCount : {_unmovableBlockCount}")
+    });
+
+    // BasicBlock
+    basicBlockView.SetActive(_basicBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"BasicBlockCount : {_basicBlockCount}")
+    });
+    doubleBasicBlockView.SetActive(_basicBlockCount switch
+    {
+        0 or 1 => false,
+        2 => true,
+        _ => throw new InvalidOperationException($"BasicBlockCount : {_basicBlockCount}")
+    });
+
+    // HeavyBlock
+    heavyBlockView.SetActive(_heavyBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"HeavyBlockCount : {_heavyBlockCount}")
+    });
+    doubleHeavyBlockView.SetActive(_heavyBlockCount switch
+    {
+        0 or 1 => false,
+        2 => true,
+        _ => throw new InvalidOperationException($"HeavyBlockCount : {_heavyBlockCount}")
+    });
+
+    // FragileBlock
+    fragileBlockView.SetActive(_fragileBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"FragileBlockCount : {_fragileBlockCount}")
+    });
+
+    // ConfusionBlock
+    confusionBlockView.SetActive(_confusionBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"ConfusionBlockCount : {_confusionBlockCount}")
+    });
+
+    // TreasureCoinBlock
+    treasureCoinView.SetActive(_treasureCoinCount switch
+    {
+        1 => true,
+        _ => false
+    });
+
+    // CannonBlock
+    cannonBlockView.SetActive(_cannonBlockCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"CannonBlockCount : {_cannonBlockCount}")
+    });
+    if (_cannonDirectionLocal != _cannonDirection)
+    {
+        _cannonDirectionLocal = _cannonDirection;
+        cannonBlockView.transform.parent.Rotate(0,CalcRotationAmount(_cannonDirectionLocal),0);
+    }
+
+    // Spike
+    spikeView.SetActive(_spikeCount switch
+    {
+        0 or 2 => false,
+        1 => true,
+        _ => throw new InvalidOperationException($"SpikeCount : {_spikeCount}")
+    });
+}
+
 
 
         public void SetInitAllEntityActiveData(IEnumerable<IEntity> allEntities)
         {
-            // // Debug.Log($"allEntities : {string.Join(", ", allEntities)}");
-            // var allEntityList = allEntities.ToList();
-            // PresentDataRef.BasicBlockCount = allEntityList.OfType<BasicBlock>().Count();
-            // PresentDataRef.UnmovableBlockCount = allEntityList.OfType<UnmovableBlock>().Count();
-            // PresentDataRef.HeavyBlockCount = allEntityList.OfType<HeavyBlock>().Count();
-            // PresentDataRef.FragileBlockCount = allEntityList.OfType<FragileBlock>().Count();
-            // PresentDataRef.CannonBlockCount = allEntityList.OfType<CannonBlock>().Count();
-            // PresentDataRef.ConfusionBlockCount = allEntityList.OfType<ConfusionBlock>().Count();
-            // PresentDataRef.TreasureCoinCount = allEntityList.OfType<TreasureCoin>().Count();
-            // PresentDataRef.CannonDirection = allEntityList.OfType<CannonBlock>().FirstOrDefault()?.KindValue switch
-            // {
-            //     CannonBlock.Kind.Up => Direction.Up,
-            //     CannonBlock.Kind.Left => Direction.Left,
-            //     CannonBlock.Kind.Down => Direction.Down,
-            //     CannonBlock.Kind.Right => Direction.Right,
-            //     null => Direction.Up,  // 存在しない場合は上向きにしておく
-            //     _ => throw new InvalidOperationException()
-            // };
-            // PresentDataRef.SpikeCount = allEntityList.OfType<Spike>().Count();
+            // Debug.Log($"allEntities : {string.Join(", ", allEntities)}");
+            var allEntityList = allEntities.ToList();
+            _basicBlockCount = allEntityList.OfType<BasicBlock>().Count();
+            _unmovableBlockCount = allEntityList.OfType<UnmovableBlock>().Count();
+            _heavyBlockCount = allEntityList.OfType<HeavyBlock>().Count();
+            _fragileBlockCount = allEntityList.OfType<FragileBlock>().Count();
+            _cannonBlockCount = allEntityList.OfType<CannonBlock>().Count();
+            _confusionBlockCount = allEntityList.OfType<ConfusionBlock>().Count();
+            _treasureCoinCount = allEntityList.OfType<TreasureCoin>().Count();
+            _cannonDirection = allEntityList.OfType<CannonBlock>().FirstOrDefault()?.KindValue switch
+            {
+                CannonBlock.Kind.Up => Direction.Up,
+                CannonBlock.Kind.Left => Direction.Left,
+                CannonBlock.Kind.Down => Direction.Down,
+                CannonBlock.Kind.Right => Direction.Right,
+                null => Direction.Up,  // 存在しない場合は上向きにしておく
+                _ => throw new InvalidOperationException()
+            };
+            _spikeCount = allEntityList.OfType<Spike>().Count();
         }
 
         public void SetEntityActiveData(IEntity entity, int count)
         {
-            // // Debug.Log($"BlockPresenterNet.SetBlockActiveData block : {block} count : {count}");
-            // switch (entity)
-            // {
-            //     case BasicBlock _:
-            //         PresentDataRef.BasicBlockCount = count;
-            //         break;
-            //     case UnmovableBlock _:
-            //         PresentDataRef.UnmovableBlockCount = count;
-            //         break;
-            //     case HeavyBlock _:
-            //         PresentDataRef.HeavyBlockCount = count;
-            //         break;
-            //     case FragileBlock _:
-            //         PresentDataRef.FragileBlockCount = count;
-            //         break;
-            //     case ConfusionBlock _:
-            //         PresentDataRef.ConfusionBlockCount = count;
-            //         break;
-            //     case TreasureCoin _:
-            //         PresentDataRef.TreasureCoinCount = count;
-            //         break;
-            //     case CannonBlock cannonBlock:
-            //         Debug.Log($"CannonBlock KindValue : {cannonBlock.KindValue} ");
-            //         PresentDataRef.CannonBlockCount = count;
-            //         PresentDataRef.CannonDirection = cannonBlock.KindValue switch
-            //         {
-            //             CannonBlock.Kind.Up => Direction.Up,
-            //             CannonBlock.Kind.Left => Direction.Left,
-            //             CannonBlock.Kind.Down => Direction.Down,
-            //             CannonBlock.Kind.Right => Direction.Right,
-            //             _ => throw new System.Exception($"想定外のCannonBlock.Kindが渡されました cannonBlock.KindValue : {cannonBlock.KindValue}")
-            //         };
-            //         break;
-            //     default:
-            //         throw new System.Exception($"想定外のEntityが渡されました block : {entity}");
-            //     case Spike _:
-            //         PresentDataRef.SpikeCount = count;
-            //         break;
-            // }
+            // Debug.Log($"BlockPresenterNet.SetBlockActiveData block : {block} count : {count}");
+            switch (entity)
+            {
+                case BasicBlock _:
+                    _basicBlockCount = count;
+                    break;
+                case UnmovableBlock _:
+                    _unmovableBlockCount = count;
+                    break;
+                case HeavyBlock _:
+                    _heavyBlockCount = count;
+                    break;
+                case FragileBlock _:
+                    _fragileBlockCount = count;
+                    break;
+                case ConfusionBlock _:
+                    _confusionBlockCount = count;
+                    break;
+                case TreasureCoin _:
+                    _treasureCoinCount = count;
+                    break;
+                case CannonBlock cannonBlock:
+                    Debug.Log($"CannonBlock KindValue : {cannonBlock.KindValue} ");
+                    _cannonBlockCount = count;
+                   _cannonDirection = cannonBlock.KindValue switch
+                    {
+                        CannonBlock.Kind.Up => Direction.Up,
+                        CannonBlock.Kind.Left => Direction.Left,
+                        CannonBlock.Kind.Down => Direction.Down,
+                        CannonBlock.Kind.Right => Direction.Right,
+                        _ => throw new System.Exception($"想定外のCannonBlock.Kindが渡されました cannonBlock.KindValue : {cannonBlock.KindValue}")
+                    };
+                    break;
+                default:
+                    throw new System.Exception($"想定外のEntityが渡されました block : {entity}");
+                case Spike _:
+                    _spikeCount = count;
+                    break;
+            }
         }
         
-        // float CalcRotationAmount(Direction direction)
-        // {
-        //     return direction switch
-        //     {
-        //         Direction.Up => 0f,
-        //         Direction.Left => -90f,
-        //         Direction.Down => -180f,
-        //         Direction.Right => 90f,
-        //         _ => throw new System.Exception($"想定外のDirectionが渡されました direction : {direction}")
-        //     };
-        // }
+        float CalcRotationAmount(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => 0f,
+                Direction.Left => -90f,
+                Direction.Down => -180f,
+                Direction.Right => 90f,
+                _ => throw new System.Exception($"想定外のDirectionが渡されました direction : {direction}")
+            };
+        }
         
-        // enum Direction
-        // {
-        //     Up,
-        //     Left,
-        //     Down,
-        //     Right
-        // }
+        enum Direction
+        {
+            Up,
+            Left,
+            Down,
+            Right
+        }
     }
 }
