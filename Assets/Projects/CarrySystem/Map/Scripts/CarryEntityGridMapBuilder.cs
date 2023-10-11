2683e2a6dd55c61287b7e379016c2bc28bd369af
suupia <morita777k@icloud.com>
2023-10-11 13:32:33 +0900

    EntityGridMapBuilder.csを削除

--- Assets/Projects/CarrySystem/Map/Scripts/CarryEntityGridMapBuilder.cs
+++ Assets/Projects/CarrySystem/Map/Scripts/CarryEntityGridMapBuilder.cs
@@ -33,8 +33,7 @@ namespace Carry.CarrySystem.Map.Scripts
                 AddEntityFromRecord<HeavyBlock, HeavyBlockRecord, HeavyBlock.Kind>(gridMapData.heavyBlockRecords, () => gridMapData.heavyBlockRecords?.Length?? 0, (record) => record.kinds, HeavyBlock.Kind.None, map, i );
                 AddEntityFromRecord<FragileBlock, FragileBlockRecord, FragileBlock.Kind>(gridMapData.fragileBlockRecords, () => gridMapData.fragileBlockRecords?.Length ?? 0, (record) => record.kinds, FragileBlock.Kind.None, map, i );
                 AddEntityFromRecord<CannonBlock, CannonBlockRecord, CannonBlock.Kind>(gridMapData.cannonBlockRecords, () => gridMapData.cannonBlockRecords?.Length ?? 0, (record) => record.kinds, CannonBlock.Kind.None, map, i );
-               　
-                // Spike
+                AddEntityFromRecord<ConfusionBlock, ConfusionBlockRecord, ConfusionBlock.Kind>(gridMapData.confusionBlockRecords, () => gridMapData.confusionBlockRecords?.Length ?? 0, (record) => record.kinds, ConfusionBlock.Kind.None, map, i );
                 AddEntityFromRecord<Spike, SpikeGimmickRecord, Spike.Kind>(gridMapData.spikeGimmickRecords, () =>gridMapData.spikeGimmickRecords?.Length?? 0, (record) => record.kinds, Spike.Kind.None, map, i );
 
                 // TreasureCoin
