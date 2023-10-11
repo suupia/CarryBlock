4853cad1768b197eec39494d253c3cf891071ad8
suupia <morita777k@icloud.com>
2023-10-11 18:09:37 +0900

    IEntityPresenterSpawnerを追加し、サブクラスを実装

--- Assets/Projects/CarrySystem/Spawners/Scripts/EntityPresenterSpawner.cs
+++ Assets/Projects/CarrySystem/Spawners/Scripts/EntityPresenterSpawner.cs
@@ -1,8 +1,30 @@
-﻿#nullable enable
+﻿using Carry.CarrySystem.Map.Interfaces;
+using Carry.CarrySystem.Map.Scripts;
+using Carry.CarrySystem.Spawners.Interfaces;
+using Carry.Utility.Interfaces;
+using Carry.Utility.Scripts;
+using Fusion;
+using UnityEngine;
+#nullable enable
+
 namespace Carry.CarrySystem.Spawners.Scripts
 {
-    public class EntityPresenterSpawner
+    public class EntityPresenterSpawner : IEntityPresenterSpawner
     {
-        
+        readonly NetworkRunner _runner;
+        readonly IPrefabLoader<EntityPresenterNet> _tilePresenterPrefabSpawner;
+
+        public EntityPresenterSpawner(NetworkRunner runner)
+        {
+            _runner = runner;
+            _tilePresenterPrefabSpawner =
+                new PrefabLoaderFromAddressable<EntityPresenterNet>("Prefabs/Map/EntityPresenter");
+        }
+
+        public IEntityPresenter SpawnPrefab(Vector3 position, Quaternion rotation)
+        {
+            var tilePresenter = _tilePresenterPrefabSpawner.Load();
+            return _runner.Spawn(tilePresenter, position, rotation, PlayerRef.None);
+        }
     }
 }
--- Assets/Projects/CarrySystem/Spawners/Scripts/LocalEntityPresenterSpawner.cs
+++ Assets/Projects/CarrySystem/Spawners/Scripts/LocalEntityPresenterSpawner.cs
@@ -1,7 +1,30 @@
-﻿namespace Carry.CarrySystem.Spawners.Scripts
+﻿using Carry.CarrySystem.Map.Interfaces;
+using Carry.CarrySystem.Map.Scripts;
+using Carry.CarrySystem.Spawners.Interfaces;
+using Carry.Utility.Interfaces;
+using Carry.Utility.Scripts;
+using Fusion;
+using UnityEngine;
+#nullable enable
+
+namespace Carry.CarrySystem.Spawners.Scripts
 {
-    public class LocalEntityPresenterSpawner
+    public class LocalEntityPresenterSpawner : IEntityPresenterSpawner
     {
-        
+        readonly NetworkRunner _runner;
+        readonly IPrefabLoader<EntityPresenterLocal> _tilePresenterPrefabSpawner;
+
+        public LocalEntityPresenterSpawner(NetworkRunner runner)
+        {
+            _runner = runner;
+            _tilePresenterPrefabSpawner =
+                new PrefabLoaderFromAddressable<EntityPresenterLocal>("Prefabs/Map/LocalEntityPresenter");
+        }
+
+        public IEntityPresenter SpawnPrefab(Vector3 position, Quaternion rotation)
+        {
+            var tilePresenter = _tilePresenterPrefabSpawner.Load();
+            return UnityEngine.Object.Instantiate(tilePresenter, position, rotation);
+        }
     }
 }
