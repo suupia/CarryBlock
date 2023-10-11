7886ec5c9af75b2a1af73cc2755b75f141145956
suupia <morita777k@icloud.com>
2023-10-11 15:47:52 +0900

    LocalEditMapInitializerやLocalEditMapScopeを作成

---
+++ Assets/Projects/EditMapSystem/EditMap/Scripts/Initializers/LocalEditMapInitializer.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+using Carry.CarrySystem.Map.Interfaces;
+using Carry.CarrySystem.Map.Scripts;
+using Cysharp.Threading.Tasks;
+using Fusion;
+using TMPro;
+using UnityEngine;
+using VContainer;
+
+namespace Carry.EditMapSystem.EditMap.Scripts
+{
+    public class LocalEditMapInitializer : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI mapKeyText;
+        IMapUpdater _editMapUpdater;
+        
+        [Inject]
+        public void Construct(
+            IMapUpdater editMapUpdater)
+        {
+            _editMapUpdater = editMapUpdater;
+
+        }
+
+        void Start()
+        {
+            _editMapUpdater.InitUpdateMap(MapKey.Default,-1); // -1が初期マップ
+
+            // 準備シーンからMapKeyを受け取る
+            // var mapKeyContainer = FindObjectOfType<MapKeyContainer>();
+            // mapKeyText.text = $"MapKey : {mapKeyContainer.MapKey.ToString()}" ;
+        }
+    }
+}
