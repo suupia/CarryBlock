83ea6c5d4312d2d338f9f37b4be9d2b9ad0d091d
Kinogas <qingmeizhongben5@gmail.com>
2023-10-11 14:51:20 +0900

    OptionCanvasMono.csを新規作成

---
+++ Assets/Projects/UISystem/UI/TitleScene/OptionCanvasMono.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Carry.UISystem.UI.TitleScene
+{
+    public class OptionCanvasMono : MonoBehaviour
+    {
+        [SerializeField] Slider bgmSlider;
+    }
+}
