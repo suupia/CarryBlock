84516301941a359d9a58486931f861070828b22c
suupia <morita777k@icloud.com>
2023-10-19 23:36:43 +0900

    EnemyControllerとEnemyのプレハブを追加

---
+++ Assets/Projects/CarrySystem/Enemy/EnemyControllerNet.cs
@@ -0,0 +1,24 @@
+﻿using Fusion;
+using UnityEngine;
+
+#nullable enable
+
+namespace Projects.CarrySystem.Enemy
+{
+    public class EnemyControllerNet : NetworkBehaviour
+    {
+        public override void Spawned()
+        {
+            // スポーンされた時に呼ばれる関数です。
+            
+            Debug.Log($"EnemyController.Spawned()");
+            
+        }
+        
+        public override void FixedUpdateNetwork()
+        {
+            // Tickごとに呼ばれる関数です。
+            // まあ、ざっくりUpdate関数みたいなものです。
+        }
+    }
+}
