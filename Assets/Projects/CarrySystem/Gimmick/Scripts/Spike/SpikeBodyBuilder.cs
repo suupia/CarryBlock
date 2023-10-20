35a075c9269b3d98d176c8055150e48b506f57ab
suupia <morita777k@icloud.com>
2023-10-20 15:31:24 +0900

    SpikeBodyControllerNetの処理を記述

--- Assets/Projects/CarrySystem/Gimmick/Scripts/Spike/SpikeBodyBuilder.cs
+++ Assets/Projects/CarrySystem/Gimmick/Scripts/Spike/SpikeBodyBuilder.cs
@@ -1,7 +1,43 @@
-﻿namespace Projects.CarrySystem.Gimmick.Scripts
+﻿using Carry.CarrySystem.Block.Scripts;
+using Carry.CarrySystem.Gimmick.Scripts;
+using Carry.Utility.Interfaces;
+using Carry.Utility.Scripts;
+using Fusion;
+using UnityEngine;
+using VContainer;
+#nullable enable
+
+namespace Projects.CarrySystem.Gimmick.Scripts
 {
     public class SpikeBodyBuilder
     {
+        readonly NetworkRunner _runner;
+        readonly IPrefabLoader<SpikeBodyControllerNet> _canonBallControllerLoader;
+
+        [Inject]
+        public SpikeBodyBuilder(NetworkRunner runner)
+        {
+            _runner = runner;
+            // I decided to not DI Loader
+            _canonBallControllerLoader = new PrefabLoaderFromAddressable<SpikeBodyControllerNet>("Prefabs/Gimmick/Cannon/CannonBallNet");
+        }
         
+        public SpikeBodyControllerNet Build(CannonBlock.Kind kind, Vector3 position, Quaternion rotation, PlayerRef playerRef)
+        {
+            // Load prefab
+            var cannonBallController = _canonBallControllerLoader.Load();
+            
+            // Spawn prefab
+            var cannonBallControllerObj = _runner.Spawn(cannonBallController, position, rotation, playerRef,
+                (runner, networkObj) =>
+                {
+                    networkObj.GetComponent<CannonBallControllerNet>().Init(kind);
+                });
+            
+            // Setup Info
+            // ...
+            
+            return cannonBallControllerObj;
+        }
     }
 }
--- Assets/Projects/CarrySystem/Gimmick/Scripts/Spike/SpikeBodyControllerNet.cs
+++ Assets/Projects/CarrySystem/Gimmick/Scripts/Spike/SpikeBodyControllerNet.cs
@@ -1,10 +1,42 @@
-﻿using Fusion;
+﻿using Carry.CarrySystem.Player.Interfaces;
+using Fusion;
+using UnityEngine;
+
 #nullable enable
 
 namespace Projects.CarrySystem.Gimmick.Scripts
-{
+{ 
+    [RequireComponent(typeof(Collider))]
     public class SpikeBodyControllerNet : NetworkBehaviour
     {
+        readonly float _lifeTime = 3f;
+
+        float _timer;
         
+        
+        public override void FixedUpdateNetwork()
+        {
+            if(!HasStateAuthority) return;
+
+            _timer += Time.deltaTime;
+            if (_timer > _lifeTime)
+            {
+                Runner.Despawn(Object);  // you have to write this code at bottom of this method
+            }
+            
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!HasStateAuthority) return;
+
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log($"痛ったぁぁぁぁい");
+                var playerController = other.GetComponent<AbstractNetworkPlayerController>();
+                var character = playerController.GetCharacter;
+                character.OnDamage();
+            }
+        }
     }
 }
