d97ed2e79c2d6ed8e3b90d91d87c281e9933f402
tei4 <ten10ru.i@gmail.com>
2023-10-25 00:49:47 +0900

    routePresenterが広がって現れるように実装

--- Assets/Projects/CarrySystem/SearchRoute/Scripts/RoutePresenter_Net.cs
+++ Assets/Projects/CarrySystem/SearchRoute/Scripts/RoutePresenter_Net.cs
@@ -12,17 +12,34 @@ namespace Carry.CarrySystem.SearchRoute.Scripts
     {
         [Networked] public NetworkBool IsActive { get; set; }
         [Networked] private NetworkBool IsAnimated { get; set; }
+        private bool _isInit = false;
 
         [SerializeField] GameObject routeHighlightObject = null!;
         
         public override void Render()
         {
             routeHighlightObject.SetActive(IsActive);
+            StartAnimation();
         }
         
         public void SetPresenterActive(bool isActive)
         {
             IsActive = isActive;
+        }
+
+        public void StartAnimation()
+        {
+            if (!IsActive)
+            {
+                IsAnimated = false;
+                return;
+            }
+            
+            if (IsAnimated) return;
+
+            routeHighlightObject.transform.localScale = Vector3.one * 0.001f;
+            routeHighlightObject.transform.DOScale(Vector3.one, 0.2f);
+            IsAnimated = true;
         }
     }
 }
