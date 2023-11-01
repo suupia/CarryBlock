be98e67a69079ce825807398c9a82cb67c3f5ff6
suupia <morita777k@icloud.com>
2023-11-01 21:46:26 +0900

    MainCarryPlayerFactoryに各インターフェースを返すメソッドを追加

--- Assets/Projects/CarrySystem/Player/Scripts/Carry/MainCarryPlayerFactory.cs
+++ Assets/Projects/CarrySystem/Player/Scripts/Carry/MainCarryPlayerFactory.cs
@@ -10,7 +10,7 @@ namespace Carry.CarrySystem.Player.Scripts
     public class MainCarryPlayerFactory : ICarryPlayerFactory
     {
         readonly IMapUpdater _mapUpdater;
-        readonly  HoldingBlockObserver  _holdingBlockObserver;
+        readonly HoldingBlockObserver  _holdingBlockObserver;
         readonly PlayerNearCartHandlerNet _playerNearCartHandler;
         readonly PlayerCharacterTransporter _playerCharacterTransporter;
         [Inject]
@@ -29,16 +29,56 @@ namespace Carry.CarrySystem.Player.Scripts
         
         public ICharacter Create(PlayerColorType colorType)
         {
+            // IMoveExecutorSwitcher
             var moveExeSwitcher = new MoveExecutorSwitcher();
+            
+            // IHoldActionExecutor
             var blockContainer = new PlayerHoldingObjectContainer();
-            var holdExe = new HoldActionExecutor(blockContainer,_playerNearCartHandler, _mapUpdater);
             _holdingBlockObserver.RegisterHoldAction(blockContainer);
+            var holdExe = new HoldActionExecutor(blockContainer,_playerNearCartHandler, _mapUpdater);
+            
+            // IOnDamageExecutor
+            var onDamageExe = new OnDamageExecutor(moveExeSwitcher, _playerCharacterTransporter);
+
+            // IDashActionExecutor
+            var dashExe = new DashExecutor(moveExeSwitcher, onDamageExe);
+
+            // IPassActionExecutor
             var passBlockMoveExe = new PassWaitExecutor();
             var passExe = new PassActionExecutor(blockContainer, passBlockMoveExe);
-            var onDamageExe = new OnDamageExecutor(moveExeSwitcher, _playerCharacterTransporter);
-            var dashExe = new DashExecutor(moveExeSwitcher, onDamageExe);
-            var character = new Character(moveExeSwitcher, holdExe,dashExe, passExe,onDamageExe, blockContainer);
+            
+            var character = new Character(moveExeSwitcher, holdExe,dashExe, passExe, onDamageExe, blockContainer);
             return character;
         }
+        
+        public IMoveExecutorSwitcher CreateMoveExecutorSwitcher()
+        {
+            return new MoveExecutorSwitcher();
+        }
+        
+        public IHoldActionExecutor CreateHoldActionExecutor()
+        {
+            var blockContainer = new PlayerHoldingObjectContainer();
+            _holdingBlockObserver.RegisterHoldAction(blockContainer);
+            return new HoldActionExecutor(blockContainer,_playerNearCartHandler, _mapUpdater);
+        } 
+                
+        public IOnDamageExecutor OnDamageExecutor(IMoveExecutorSwitcher moveExecutorSwitcher)
+        {
+            return new OnDamageExecutor(moveExecutorSwitcher, _playerCharacterTransporter);
+        }
+        
+        public IDashExecutor CreateDashExecutor(IMoveExecutorSwitcher moveExecutorSwitcher, IOnDamageExecutor onDamageExecutor)
+        {
+            return new DashExecutor(moveExecutorSwitcher, onDamageExecutor);
+        }
+        
+        public IPassActionExecutor CreatePassActionExecutor(PlayerHoldingObjectContainer blockContainer)
+        {
+            var passBlockMoveExe = new PassWaitExecutor();
+            return new PassActionExecutor(blockContainer, passBlockMoveExe);
+        }
+
+
     }
 }
