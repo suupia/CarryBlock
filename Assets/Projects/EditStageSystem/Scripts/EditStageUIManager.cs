cd70ef93a3b5017acac74f7d27450a88169be1e0
tyPhoon <typhoonmoon1022@gmail.com>
2023-11-05 12:58:48 +0900

    ステージ選択のためのUIの追加

---
+++ Assets/Projects/EditStageSystem/Scripts/EditStageUIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class EditStageUIManager : MonoBehaviour
+{
+    [SerializeField] List<Button> stageButtons;
+    [SerializeField] List<Button> mapButtons;
+
+    [SerializeField] GameObject detailPanel;
+
+    [SerializeField] Button detailPanelBackButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        detailPanel.SetActive(false);
+        detailPanelBackButton.onClick.AddListener(() => detailPanel.SetActive(false));
+        
+        stageButtons.ForEach(stage => stage.onClick.AddListener(() => detailPanel.SetActive(true)));
+        
+        mapButtons.ForEach(map => map.onClick.AddListener(() => SceneManager.LoadScene("LocalEditMapScene")));
+    }
+}
