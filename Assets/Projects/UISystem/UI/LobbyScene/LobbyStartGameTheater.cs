7d9c0429c0e25d1711bae04ea6c09bcf9ead12e4
suupia <morita777k@icloud.com>
2023-11-09 00:03:35 +0900

    LobbyStartGameTheaterを完成

--- Assets/Projects/UISystem/UI/LobbyScene/LobbyStartGameTheater.cs
+++ Assets/Projects/UISystem/UI/LobbyScene/LobbyStartGameTheater.cs
@@ -1,4 +1,5 @@
-﻿using Carry.CarrySystem.Cart.Scripts;
+﻿using System;
+using Carry.CarrySystem.Cart.Scripts;
 using Carry.CarrySystem.Player.Scripts;
 using Projects.CarrySystem.Enemy;
 using VContainer;
@@ -11,101 +12,35 @@ namespace Carry.UISystem.UI.LobbyScene
 {
     public class LobbyStartGameTheater
     {
-        CartLobbyControllerNet cart;
-        EnemyControllerNet enemy;
-        LobbyPlayerContainer _lobbyPlayerContainer = null!;
+        readonly CartLobbyControllerNet _cart;
+        readonly EnemyControllerNet _enemy;
+        readonly LobbyPlayerContainer _lobbyPlayerContainer = null!;
 
 
         [Inject]
         public LobbyStartGameTheater(CartLobbyControllerNet cart, EnemyControllerNet enemy)
         {
-            this.cart = cart;
-            this.enemy = enemy;
+            _cart = cart;
+            _enemy = enemy;
         }
 
-        void Hoge()
+        public void PlayLobbyTheater(Action onCompleteAction)
         {
-            var enemyAnimatorPresenter = enemy.GetComponentInChildren<EnemyAnimatorPresenterNet>();
+            var enemyAnimatorPresenter = _enemy.GetComponentInChildren<EnemyAnimatorPresenterNet>();
 
             var animationSequence = DOTween.Sequence();
 
-            // プレイヤーがカートに乗るアニメーション
             animationSequence = AppendPlayerGetIntoCartAnimation(animationSequence);
 
-            // enemy Animation
-            animationSequence = AppendEnemyApproachAnimation(animationSequence);
+            animationSequence = AppendEnemyApproachAnimation(animationSequence, enemyAnimatorPresenter);
 
-            // player LookAt
             animationSequence = AppendPlayerLookAtEnemyAnimation(animationSequence);
 
-            // enemy chase and cart move
-            animationSequence = AppendEnemyAndCartMoveAnimation(animationSequence);
+            animationSequence = AppendEnemyAndCartMoveAnimation(animationSequence, enemyAnimatorPresenter);
+
+            animationSequence.OnComplete(() => onCompleteAction());
 
             animationSequence.Play();
-
-            // 以下はクラスに分割する前準備としてのローカル関数
-
-            Sequence AppendPlayerGetIntoCartAnimation(Sequence sequence)
-            {
-                for (int playerIndex = 0;
-                     playerIndex < _lobbyPlayerContainer.PlayerControllers.Count;
-                     playerIndex++)
-                {
-                    var playerController = _lobbyPlayerContainer.PlayerControllers[playerIndex];
-                    var playerTransform = playerController.transform;
-
-                    sequence
-                        .Append(playerTransform.DOLookAt(
-                            CalcPlayerPositionInCart(playerIndex, cart.transform.position), 0))
-                        .AppendCallback(() => playerController.GetCharacter.Dash())
-                        .Append(playerTransform.DOMove(
-                            CalcPlayerPositionInCart(playerIndex, cart.transform.position), 2f))
-                        .AppendCallback(() =>
-                        {
-                            playerController.GetCharacter.SwitchToRegularMove();
-                            var changePosition = new Vector3(playerTransform.position.x, 1.0f,
-                                playerTransform.position.z);
-                            playerTransform.position = changePosition;
-                            playerTransform.SetParent(cart.transform);
-                        });
-                }
-
-                return sequence;
-            }
-
-            Sequence AppendEnemyApproachAnimation(Sequence sequence)
-            {
-                sequence
-                    .Append(enemy.transform.DOLookAt(cart.transform.position, 0))
-                    .AppendCallback(() => enemyAnimatorPresenter.Chase())
-                    .Append(enemy.transform.DOMove(new Vector3(-6f, 0, 0f), 1.5f))
-                    .AppendCallback(() => enemyAnimatorPresenter.Threat())
-                    .Append(enemy.transform.DOMove(new Vector3(-5.5f, 0, 0f), 1.5f));
-                return sequence;
-            }
-
-            Sequence AppendPlayerLookAtEnemyAnimation(Sequence sequence)
-            {
-                foreach (var playerController in _lobbyPlayerContainer.PlayerControllers)
-                {
-                    sequence.Append(playerController.transform.DOLookAt(new Vector3(40f, 0, 0), 0));
-                }
-
-                return sequence;
-            }
-
-            Sequence AppendEnemyAndCartMoveAnimation(Sequence sequence)
-            {
-                sequence
-                    .AppendCallback(() => enemyAnimatorPresenter.Chase())
-                    .Append(enemy.transform.DOMove(new Vector3(15f, 0, 0f), 2.5f))
-                    .Join(cart.transform.DOMove(new Vector3(40f, 0, 0), 2.5f))
-                    .OnComplete(() =>
-                    {
-                        // ボタン押されたときに本当にやりたい処理をここで実行
-                    });
-                return sequence;
-            }
         }
 
         Vector3 CalcPlayerPositionInCart(int playerIndex, Vector3 cartPosition)
@@ -118,6 +53,64 @@ namespace Carry.UISystem.UI.LobbyScene
                 3 => cartPosition + new Vector3(-0.7f, 0, -0.7f),
                 _ => throw new System.ArgumentOutOfRangeException()
             };
+        }
+
+
+        Sequence AppendPlayerGetIntoCartAnimation(Sequence sequence)
+        {
+            for (int playerIndex = 0;
+                 playerIndex < _lobbyPlayerContainer.PlayerControllers.Count;
+                 playerIndex++)
+            {
+                var playerController = _lobbyPlayerContainer.PlayerControllers[playerIndex];
+                var playerTransform = playerController.transform;
+
+                sequence
+                    .Append(playerTransform.DOLookAt(
+                        CalcPlayerPositionInCart(playerIndex, _cart.transform.position), 0))
+                    .AppendCallback(() => playerController.GetCharacter.Dash())
+                    .Append(playerTransform.DOMove(
+                        CalcPlayerPositionInCart(playerIndex, _cart.transform.position), 2f))
+                    .AppendCallback(() =>
+                    {
+                        playerController.GetCharacter.SwitchToRegularMove();
+                        var changePosition = new Vector3(playerTransform.position.x, 1.0f, playerTransform.position.z);
+                        playerTransform.position = changePosition;
+                        playerTransform.SetParent(_cart.transform);
+                    });
+            }
+
+            return sequence;
+        }
+
+        Sequence AppendEnemyApproachAnimation(Sequence sequence, EnemyAnimatorPresenterNet enemyAnimatorPresenter)
+        {
+            sequence
+                .Append(_enemy.transform.DOLookAt(_cart.transform.position, 0))
+                .AppendCallback(() => enemyAnimatorPresenter.Chase())
+                .Append(_enemy.transform.DOMove(new Vector3(-6f, 0, 0f), 1.5f))
+                .AppendCallback(() => enemyAnimatorPresenter.Threat())
+                .Append(_enemy.transform.DOMove(new Vector3(-5.5f, 0, 0f), 1.5f));
+            return sequence;
+        }
+
+        Sequence AppendPlayerLookAtEnemyAnimation(Sequence sequence)
+        {
+            foreach (var playerController in _lobbyPlayerContainer.PlayerControllers)
+            {
+                sequence.Append(playerController.transform.DOLookAt(new Vector3(40f, 0, 0), 0));
+            }
+
+            return sequence;
+        }
+
+        Sequence AppendEnemyAndCartMoveAnimation(Sequence sequence, EnemyAnimatorPresenterNet enemyAnimatorPresenter)
+        {
+            sequence
+                .AppendCallback(() => enemyAnimatorPresenter.Chase())
+                .Append(_enemy.transform.DOMove(new Vector3(15f, 0, 0f), 2.5f))
+                .Join(_cart.transform.DOMove(new Vector3(40f, 0, 0), 2.5f));
+            return sequence;
         }
     }
 }
