283c8adb7fd7c06d261189d407e630362ac76e46
tyPhoon <typhoonmoon1022@gmail.com>
2023-11-15 00:45:34 +0900

    ステージの概念を導入、LocalEditStageSceneにて、初回セーブ、ロードの実装

---
+++ Assets/Projects/CarrySystem/Map/Scripts/StageLoader.cs
@@ -0,0 +1,148 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Carry.CarrySystem.Map.Scripts
+{
+    [Serializable]
+    public struct Stage
+    {
+        public Stage(string name)
+        {
+            this.name = name;
+            id = Guid.NewGuid().ToString();
+            mapInfos = new List<MapInfo>
+            {
+                new("map1"),
+                new("map2"),
+                new("map3"),
+                new("map4"),
+                new("map5"),
+            };
+        } 
+        
+        // public string version;
+        public string name;
+        public string id;
+        public List<MapInfo> mapInfos;
+    }
+
+    [Serializable]
+    public struct MapInfo
+    {
+        public MapInfo(string name)
+        {
+            this.name = name;
+            using var streamReader = new StreamReader(EntityGridMapFileUtility.GetDefaultFilePath());
+            var stringData = streamReader.ReadToEnd();
+            streamReader.Close();
+            data = JsonUtility.FromJson<EntityGridMapData>(stringData);
+        }
+        public string name;
+        public EntityGridMapData data;
+    }
+
+    [Serializable]
+    struct World
+    {
+        public List<string> stageIds;
+    }
+
+    public static class StageIOSystem
+    {
+        static string GetPath(string stageId) => Path.Combine(
+            Application.streamingAssetsPath, "JsonFiles", "Stages", $"{stageId}.json");
+
+        static World GetWorld()
+        {
+            if (PlayerPrefs.HasKey("world"))
+            {
+                return JsonUtility.FromJson<World>(PlayerPrefs.GetString("world"));
+            }
+
+            var stages = SaveInitStages();
+            var world = new World
+            {
+                stageIds = stages.Select(s => s.id).ToList()
+            };
+            PlayerPrefs.SetString("world",JsonUtility.ToJson(world));
+            PlayerPrefs.Save();
+
+            return world;
+        }
+
+        public static IReadOnlyList<Stage> GetStages()
+        {
+            var world = GetWorld();
+            var stageIds = world.stageIds;
+            var requireRefreshWorld = false;
+            var stages = new List<Stage>();
+
+            for (var i = 0; i < stageIds.Count; i++)
+            {
+                var stage = Load(stageIds[i]);
+                if (stage == null)
+                {
+                    stage = new Stage($"Stage{i}");
+                    world.stageIds[i] = stage.Value.id;
+                    requireRefreshWorld = true;
+                }
+                stages.Add(stage.Value);
+            }
+
+            if (requireRefreshWorld)
+            {
+                PlayerPrefs.SetString("world",JsonUtility.ToJson(world));
+                PlayerPrefs.Save();
+            }
+            
+            return stages;
+        }
+
+        static IEnumerable<Stage> SaveInitStages()
+        {
+            var stages = new List<Stage>
+            {
+                new("Stage1"),
+                new("Stage2"),
+                new("Stage3"),
+            };
+                
+            foreach (var stage in stages)
+            {
+                Save(stage);
+            }
+            
+            Debug.Log("ステージが新規作成されました");
+
+            return stages;
+        }
+
+        static void Save(Stage stage)
+        {
+            var path = GetPath(stage.id);
+
+            var jsonData = JsonUtility.ToJson(stage);
+
+            // JSONデータをファイルに書き込む
+            using var streamWriter = new StreamWriter(path);
+            streamWriter.Write(jsonData);
+            streamWriter.Flush();
+        }
+        
+        static Stage? Load(string stageId)
+         {
+             var path = GetPath(stageId);
+
+             if (File.Exists(path))
+             {
+                 string jsonData = File.ReadAllText(path);
+                 return JsonUtility.FromJson<Stage>(jsonData);
+             }
+             Debug.LogError("File not found.");
+             return null;
+         }
+    }
+}
--- Assets/Projects/UISystem/UI/EditStage/EditStageUIManager.cs
+++ Assets/Projects/UISystem/UI/EditStage/EditStageUIManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Carry.CarrySystem.Map.Scripts;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -16,11 +19,50 @@ public class EditStageUIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         detailPanel.SetActive(false);
         detailPanelBackButton.onClick.AddListener(() => detailPanel.SetActive(false));
         
-        stageButtons.ForEach(stage => stage.onClick.AddListener(() => detailPanel.SetActive(true)));
+        SetUpStageButtons();
         
-        mapButtons.ForEach(map => map.onClick.AddListener(() => SceneManager.LoadScene("EditMapForPlayerScene")));
+    }
+
+    void SetUpStageButtons()
+    {
+        var stages = StageIOSystem.GetStages();
+
+        Assert.AreEqual(
+            stages.Count, stageButtons.Count, 
+            "UIに表示できるステージの数と実際に保存されているステージの数が一致しません"
+        );
+        
+        for (var i = 0; i < stageButtons.Count; i++)
+        {
+            var stageButton = stageButtons[i];
+            var stage = stages[i];
+            var stageButtonText = stageButton.GetComponentInChildren<TMP_Text>();
+
+            Assert.IsNotNull(stageButtonText);
+            
+            stageButtonText.text = stage.name;
+            stageButton.onClick.AddListener(() =>
+            {
+                //ステージをタッチするごとにマップを読み込み反映させる
+                //detailPanelを再利用するイメージ
+                for (var j = 0; j < mapButtons.Count; j++)
+                {
+                    var mapButton = mapButtons[j];
+                    var mapInfo = stage.mapInfos[j];
+                    var mapButtonText = mapButton.GetComponentInChildren<TMP_Text>();
+                    
+                    Assert.IsNotNull(mapButtonText);
+
+                    mapButtonText.text = mapInfo.name;
+                    mapButton.onClick.AddListener(() => SceneManager.LoadScene("EditMapForPlayerScene"));
+                }
+
+                detailPanel.SetActive(true);
+            });
+        }
     }
 }
