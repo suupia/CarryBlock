ab22ee9aae4a29acb8b25bf9099e874a5c1f3205
suupia <morita777k@icloud.com>
2023-11-18 11:11:00 +0900

    IEntityPresenterにGetMonoBehaviourを追加

--- Assets/Projects/CarrySystem/Map/Interfaces/IEntityPresenter.cs
+++ Assets/Projects/CarrySystem/Map/Interfaces/IEntityPresenter.cs
@@ -2,12 +2,15 @@
 using Carry.CarrySystem.Block.Interfaces;
 using Carry.CarrySystem.Entity.Interfaces;
 using Carry.CarrySystem.Map.Scripts;
+using UnityEngine;
+
 #nullable enable
 
 namespace Carry.CarrySystem.Map.Interfaces
 {
-    public interface IEntityPresenter
+    public interface  IEntityPresenter
     {
+        public MonoBehaviour GetMonoBehaviour { get; }
         public void SetInitAllEntityActiveData(IEnumerable<IEntity> allEntities);
         public void SetEntityActiveData(IEntity entity, int count);
         public void DestroyPresenter();
