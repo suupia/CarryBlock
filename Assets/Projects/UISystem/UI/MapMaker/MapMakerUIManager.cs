aab398a49ef9c71cb8d4a22bc3500804fc346d84
tyPhoon <typhoonmoon1022@gmail.com>
2023-11-20 22:26:53 +0900

    キャンバスの切り替えの実装の修正

--- Assets/Projects/UISystem/UI/MapMaker/MapMakerUIManager.cs
+++ Assets/Projects/UISystem/UI/MapMaker/MapMakerUIManager.cs
@@ -1,12 +1,14 @@
+#nullable enable
 using DG.Tweening;
 using Projects.MapMakerSystem.Scripts;
 using UnityEngine;
 using VContainer;
-#nullable enable
+
 //仮クラス。長谷川くんのスクリプトと統合するかも
 public class MapMakerUIManager : MonoBehaviour
 {
     [SerializeField] GameObject playingCanvas;
+    [SerializeField] GameObject editingCanvas;
     [SerializeField] Transform editingCameraTransform;
     [SerializeField] Transform testPlayingCameraTransform;
 
@@ -15,11 +17,9 @@ public class MapMakerUIManager : MonoBehaviour
 
     MapTestPlayStarter _mapTestPlayStarter = null!;
     Camera _camera;
-
-
+    
     [Inject]
-    public void Construct(
-        MapTestPlayStarter mapTestPlayStarter)
+    public void Construct(MapTestPlayStarter mapTestPlayStarter)
     {
         _mapTestPlayStarter = mapTestPlayStarter;
     }
@@ -27,9 +27,8 @@ public class MapMakerUIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playingCanvas.SetActive(false);
         _camera = Camera.main;
-        SwitchCameraToEditing();
+        SwitchToEditing();
     }
 
 
@@ -38,17 +37,26 @@ public class MapMakerUIManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            var canPlay = _mapTestPlayStarter.Start(() =>
-            {
-                playingCanvas.SetActive(false);
-                SwitchCameraToEditing();
-            });
+            var canPlay = _mapTestPlayStarter.Start(SwitchToEditing);
             if (canPlay)
             {
-                playingCanvas.SetActive(true);
-                SwitchCameraToTestPlaying();
+                SwitchToTestPlaying();
             }
         }
+    }
+
+    void SwitchToEditing()
+    {
+        editingCanvas.SetActive(true);
+        playingCanvas.SetActive(false);
+        SwitchCameraToEditing();
+    }
+
+    void SwitchToTestPlaying()
+    {
+        editingCanvas.SetActive(false);
+        playingCanvas.SetActive(true);
+        SwitchCameraToTestPlaying();
     }
 
     void SwitchCameraToEditing()
