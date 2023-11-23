6d02eec6bcfb80c1168e6d01d8f69cfec36a7631
Hasegawa Taisei <hasehasehase61@gmail.com>
2023-11-23 16:56:44 +0900

    カメラ位置の調整

--- Assets/Projects/EditMapSystem/EditMapForPlayer/Scripts/SpawnAreaPositionSetter.cs
+++ Assets/Projects/EditMapSystem/EditMapForPlayer/Scripts/SpawnAreaPositionSetter.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 
 public class SpawnAreaPositionSetter : MonoBehaviour
 {
-    [SerializeField] RectTransform _spawnAreaRectTransform = null!;
+    [SerializeField] RectTransform spawnAreaRectTransform = null!;
     
-    readonly Vector2Int _respawnAreaOrigin = new Vector2Int(0,4);
+    readonly Vector2Int _respawnAreaOrigin = new Vector2Int(1,5);   //中心の座標, ～Input.csとは違うとり方をしているので注意
+    
+    readonly int _respawnAreaLength = 3;
 
     void Start()
     {
-        // Set SpawnArea Position
-        var spawnAreaPosition = GridConverter.GridPositionToWorldPosition(_respawnAreaOrigin);
+        var spawnAreaPosWorld = GridConverter.GridPositionToWorldPosition(_respawnAreaOrigin);
+
+        var spawnAreaPosScreen = Camera.main.WorldToScreenPoint(spawnAreaPosWorld);
+
+        Debug.Log($"SpawnAreaPositionWorld: {spawnAreaPosWorld}");
+        Debug.Log($"SpawnAreaPositionScreen: {spawnAreaPosScreen}");
+
+        spawnAreaRectTransform.position = spawnAreaPosScreen;
         
-        var spawnArea = Camera.main.WorldToScreenPoint(spawnAreaPosition);
+        //スクリーン座標でのサイズを計算
+        var respawnAreaScreen = Camera.main.WorldToScreenPoint(
+                                    GridConverter.GridPositionToWorldPosition(_respawnAreaOrigin + new Vector2Int(1, 1))
+                                ) - 
+                                spawnAreaPosScreen;
         
-        // Set SpawnArea transform
-        _spawnAreaRectTransform.position = spawnArea;
+        spawnAreaRectTransform.sizeDelta = respawnAreaScreen * (_respawnAreaLength / 2.0f);
     }
 }
