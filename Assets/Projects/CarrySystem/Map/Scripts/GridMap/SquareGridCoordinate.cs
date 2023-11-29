ef9763ece7d6f177c1545a9014dbe284cc88efaf
suupia <morita777k@icloud.com>
2023-11-29 15:46:35 +0900

    Implement SquareGridCoordinate as child of IGridCoordinate

--- Assets/Projects/CarrySystem/Map/Scripts/GridMap/SquareGridCoordinate.cs
+++ Assets/Projects/CarrySystem/Map/Scripts/GridMap/SquareGridCoordinate.cs
@@ -1,10 +1,10 @@
 ﻿#nullable enable
 using Carry.CarrySystem.Map.Interfaces;
 using UnityEngine;
-
+using Debug = System.Diagnostics.Debug;
 namespace Carry.CarrySystem.Map.Scripts
 {
-    public sealed class SquareGridCoordinate
+    public sealed class SquareGridCoordinate : IGridCoordinate
     {
         int Width { get; }
         int Height { get; }
@@ -14,15 +14,10 @@ namespace Carry.CarrySystem.Map.Scripts
             Width = width;
             Height = height;
         }
-
-        // 変換メソッド
+        
         public int ToSubscript(int x, int y)
         {
-            if (IsOutOfIncludeEdgeArea(x, y))
-            {
-                Debug.LogError($"IsOutOfIncludeEdgeArea({x}, {y})がtrueです");
-                return -1;
-            }
+            Debug.Assert(IsInDataOrEdgeArea(x,y));
 
             return x + Width * y;
         }
@@ -33,79 +28,24 @@ namespace Carry.CarrySystem.Map.Scripts
             int y = subscript / Width;
             return new Vector2Int(x, y);
         }
-
-
-        /// <summary>
-        /// dataArea(座標(0,0)～(mapWidth-1,mapHeight-1)のデータが存在する領域)の内側であればtrueを返す
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
+        
         public bool IsInDataArea(int x, int y)
         {
-            return !IsOutOfDataArea(x, y);
+            if (x < 0 || Width <= x) return false;
+            if (y < 0 || Height <= y) return false;
+            return true;
         }
 
-        public bool IsInDataArea(Vector2Int vector)
-        {
-            return IsInDataArea(vector.x, vector.y);
-        }
-
-        /// <summary>
-        /// edgeArea(データは存在しないが、_initValueを返す領域)の内側であればtrueを返す
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
         public bool IsInDataOrEdgeArea(int x, int y)
         {
-            return !IsOutOfIncludeEdgeArea(x, y);
+            if (x < -1 || Width < x) return false;
+            if (y < -1 || Height < y) return false;
+            return true;
         }
 
-        public bool IsInDataOrEdgeArea(Vector2Int vector)
-        {
-            return IsInDataOrEdgeArea(vector.x, vector.y);
-        }
-
-
-        // 子クラスでエラーを出すための判定用メソッド
-        // クライアント側でIsInIncludeEdgeAreaやIsInDataRangeAreaの呼び出しを忘れないようにエラーを投げるときに使う
-
-        /// <summary>
-        /// edgeArea(データは存在しないが、_initValueを返す領域)の外側であればtrueを返す
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
         public bool IsOutOfIncludeEdgeArea(int x, int y)
         {
-            if (x < -1 || Width < x) return true;
-            if (y < -1 || Height < y) return true;
-            return false;
-        }
-
-        /// <summary>
-        /// edgeArea(データは存在しないが、_initValueを返す領域)の上であればtrueを返す
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        public bool IsOnTheEdgeArea(int x, int y)
-        {
-            if ((x == -1 || x == Width) && (-1 <= y && y <= Height)) return true; //左右の両端
-            if ((y == -1 || y == Height) && (-1 <= x && x <= Width)) return true; //上下の両端
-            return false;
-        }
-
-        /// <summary>
-        /// dataArea(座標(0,0)～(mapWidth-1,mapHeight-1)のデータが存在する領域)の外側であればtrueを返す
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        public bool IsOutOfDataArea(int x, int y)
-        {
-            return IsOutOfIncludeEdgeArea(x, y) || IsOnTheEdgeArea(x, y);
+            return !IsInDataOrEdgeArea(x, y);
         }
     }
 }
